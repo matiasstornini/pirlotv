92a5b7ff8e1a68efc3cbc616d3310fc29b0777d1
adiccfut <37271131+adiccfut@users.noreply.github.com>
2020-09-29 14:06:05 -0300

    Add files via upload

---
+++ html/vars.js
@@ -0,0 +1,3 @@
+console.log("hola");
+
+let esp = ["https://pirlotv-redirecciones.blogspot.com/2020/09/esp-0-m3u8.html","https://pirlotv-redirecciones.blogspot.com/2020/09/esp-0b-m3u8.html","https://pirlotv-redirecciones.blogspot.com/2020/09/esp-1-m3u8.html"];
