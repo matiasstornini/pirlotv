bf189e471c64acaf91a4999cbfc9fda130b46483
adiccfut <37271131+adiccfut@users.noreply.github.com>
2020-09-29 18:32:27 -0300

    Update vars.js

--- html/vars.js
+++ html/vars.js
@@ -2,3 +2,9 @@ console.log("hola");
 
 let esp = ["https://pirlotv-redirecciones.blogspot.com/2020/09/esp-0-m3u8.html","https://pirlotv-redirecciones.blogspot.com/2020/09/esp-0b-m3u8.html","https://pirlotv-redirecciones.blogspot.com/2020/09/esp-1-m3u8.html"];
 let ing = ["https://pirlotv-redirecciones.blogspot.com/2020/09/premier-0-m3u8.html","https://pirlotv-redirecciones.blogspot.com/2020/09/premier-0b-m3u8.html","https://pirlotv-redirecciones.blogspot.com/2020/09/premier-1-m3u8.html"];
+let lib = [
+"https://pirlotv-redirecciones.blogspot.com/2020/09/libertadores-href-0.html","https://pirlotv-redirecciones.blogspot.com/2020/09/libertadores-href-0b.html","https://pirlotv-redirecciones.blogspot.com/2020/09/libertadores-href-0c.html",
+"https://pirlotv-redirecciones.blogspot.com/2020/09/libertadores-href-1.html","https://pirlotv-redirecciones.blogspot.com/2020/09/libertadores-href-1b.html","https://pirlotv-redirecciones.blogspot.com/2020/09/libertadores-href-1c.html",
+"https://pirlotv-redirecciones.blogspot.com/2020/09/libertadores-href-2.html","https://pirlotv-redirecciones.blogspot.com/2020/09/libertadores-href-2b.html","https://pirlotv-redirecciones.blogspot.com/2020/09/libertadores-href-2c.html",
+"https://pirlotv-redirecciones.blogspot.com/2020/09/libertadores-href-3.html","https://pirlotv-redirecciones.blogspot.com/2020/09/libertadores-href-3b.html","https://pirlotv-redirecciones.blogspot.com/2020/09/libertadores-href-3c.html"
+];
