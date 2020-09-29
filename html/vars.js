2ac033ba8f884c534af092d6849601d87758fdcc
adiccfut <37271131+adiccfut@users.noreply.github.com>
2020-09-29 15:44:00 -0300

    Update vars.js

--- html/vars.js
+++ html/vars.js
@@ -1,4 +1,4 @@
 console.log("hola");
 
 let esp = ["https://pirlotv-redirecciones.blogspot.com/2020/09/esp-0-m3u8.html","https://pirlotv-redirecciones.blogspot.com/2020/09/esp-0b-m3u8.html","https://pirlotv-redirecciones.blogspot.com/2020/09/esp-1-m3u8.html"];
-let ing = ["https://pirlotv-redirecciones.blogspot.com/2020/09/premier-0-m3u8.html","https://pirlotv-redirecciones.blogspot.com/2020/09/premier-0b-m3u8.html","https://pirlotv-redirecciones.blogspot.com/2020/09/esp-1-m3u8.html"];
+let ing = ["https://pirlotv-redirecciones.blogspot.com/2020/09/premier-0-m3u8.html","https://pirlotv-redirecciones.blogspot.com/2020/09/premier-0b-m3u8.html","https://pirlotv-redirecciones.blogspot.com/2020/09/premier-1-m3u8.html"];
