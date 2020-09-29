1ce37188a073afa49807a031901e6868fae68d26
adiccfut <37271131+adiccfut@users.noreply.github.com>
2020-09-29 18:39:32 -0300

    Update vars.js

--- html/vars.js
+++ html/vars.js
@@ -1,10 +1,11 @@
 console.log("hola");
+var blog = "https://pirlotv-redirecciones.blogspot.com/2020/09/";
 
-let esp = ["https://pirlotv-redirecciones.blogspot.com/2020/09/esp-0-m3u8.html","https://pirlotv-redirecciones.blogspot.com/2020/09/esp-0b-m3u8.html","https://pirlotv-redirecciones.blogspot.com/2020/09/esp-1-m3u8.html"];
-let ing = ["https://pirlotv-redirecciones.blogspot.com/2020/09/premier-0-m3u8.html","https://pirlotv-redirecciones.blogspot.com/2020/09/premier-0b-m3u8.html","https://pirlotv-redirecciones.blogspot.com/2020/09/premier-1-m3u8.html"];
+let esp = [""+blog+"esp-0-m3u8.html",""+blog+"esp-0b-m3u8.html",""+blog+"esp-1-m3u8.html"];
+let ing = [""+blog+"premier-0-m3u8.html",""+blog+"premier-0b-m3u8.html",""+blog+"premier-1-m3u8.html"];
 let lib = [
-"https://pirlotv-redirecciones.blogspot.com/2020/09/libertadores-href-0.html","https://pirlotv-redirecciones.blogspot.com/2020/09/libertadores-href-0b.html","https://pirlotv-redirecciones.blogspot.com/2020/09/libertadores-href-0c.html",
-"https://pirlotv-redirecciones.blogspot.com/2020/09/libertadores-href-1.html","https://pirlotv-redirecciones.blogspot.com/2020/09/libertadores-href-1b.html","https://pirlotv-redirecciones.blogspot.com/2020/09/libertadores-href-1c.html",
-"https://pirlotv-redirecciones.blogspot.com/2020/09/libertadores-href-2.html","https://pirlotv-redirecciones.blogspot.com/2020/09/libertadores-href-2b.html","https://pirlotv-redirecciones.blogspot.com/2020/09/libertadores-href-2c.html",
-"https://pirlotv-redirecciones.blogspot.com/2020/09/libertadores-href-3.html","https://pirlotv-redirecciones.blogspot.com/2020/09/libertadores-href-3b.html","https://pirlotv-redirecciones.blogspot.com/2020/09/libertadores-href-3c.html"
+""+blog+"libertadores-href-0.html",""+blog+"libertadores-href-0b.html",""+blog+"libertadores-href-0c.html",
+""+blog+"libertadores-href-1.html",""+blog+"libertadores-href-1b.html",""+blog+"libertadores-href-1c.html",
+""+blog+"libertadores-href-2.html",""+blog+"libertadores-href-2b.html",""+blog+"libertadores-href-2c.html",
+""+blog+"libertadores-href-3.html",""+blog+"libertadores-href-3b.html",""+blog+"libertadores-href-3c.html"
 ];
