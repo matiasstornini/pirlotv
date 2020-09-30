34a42779af965380fd962be84442e10f2ab01647
adiccfut <37271131+adiccfut@users.noreply.github.com>
2020-09-29 21:08:06 -0300

    Update vars.js

--- html/vars.js
+++ html/vars.js
@@ -4,8 +4,8 @@ var blog = "https://pirlotv-redirecciones.blogspot.com/2020/09/";
 let esp = [""+blog+"esp-0-m3u8.html",""+blog+"esp-0b-m3u8.html",""+blog+"esp-1-m3u8.html"];
 let ing = [""+blog+"premier-0-m3u8.html",""+blog+"premier-0b-m3u8.html",""+blog+"premier-1-m3u8.html"];
 let lib = [
-""+blog+"libertadores-href-0.html",""+blog+"libertadores-href-0b.html",""+blog+"libertadores-href-0c.html",
 ""+blog+"libertadores-href-1.html",""+blog+"libertadores-href-1b.html",""+blog+"libertadores-href-1c.html",
+""+blog+"libertadores-href-0.html",""+blog+"libertadores-href-0b.html",""+blog+"libertadores-href-0c.html",
 ""+blog+"libertadores-href-2.html",""+blog+"libertadores-href-2b.html",""+blog+"libertadores-href-2c.html",
 ""+blog+"libertadores-href-3.html",""+blog+"libertadores-href-3b.html",""+blog+"libertadores-href-3c.html"
 ];
