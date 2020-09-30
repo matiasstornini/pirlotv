e55ac420559a87896e1cc0e1e0ad5ee1abac989d
adiccfut <37271131+adiccfut@users.noreply.github.com>
2020-09-30 18:40:56 -0300

    Add files via upload

--- html/vars.js
+++ html/vars.js
@@ -30,8 +30,8 @@ let ale = [
 ];
 
 let lib = [
-""+blog+"libertadores-href-1.html",""+blog+"libertadores-href-1b.html",""+blog+"libertadores-href-1c.html",
 ""+blog+"libertadores-href-0.html",""+blog+"libertadores-href-0b.html",""+blog+"libertadores-href-0c.html",
+""+blog+"libertadores-href-1.html",""+blog+"libertadores-href-1b.html",""+blog+"libertadores-href-1c.html",
 ""+blog+"libertadores-href-2.html",""+blog+"libertadores-href-2b.html",""+blog+"libertadores-href-2c.html",
 ""+blog+"libertadores-href-3.html",""+blog+"libertadores-href-3b.html",""+blog+"libertadores-href-3c.html"
 ];
