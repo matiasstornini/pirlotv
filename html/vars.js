812afbf55e0e19606ff1ecdf7892940fc4f3bdce
adiccfut <37271131+adiccfut@users.noreply.github.com>
2020-09-30 12:08:26 -0300

    Add files via upload

--- html/vars.js
+++ html/vars.js
@@ -3,6 +3,8 @@ var blog = "https://pirlotv-redirecciones.blogspot.com/2020/09/";
 
 let esp = [""+blog+"esp-0-m3u8.html",""+blog+"esp-0b-m3u8.html",""+blog+"esp-1-m3u8.html"];
 let ing = [""+blog+"premier-0-m3u8.html",""+blog+"premier-0b-m3u8.html",""+blog+"premier-1-m3u8.html"];
+let ita = [""+blog+"seriea-0-m3u8.html",""+blog+"seriea-0b-m3u8.html",""+blog+"seriea-1-m3u8.html",""+blog+"seriea-1b-m3u8.html"];
+
 let lib = [
 ""+blog+"libertadores-href-1.html",""+blog+"libertadores-href-1b.html",""+blog+"libertadores-href-1c.html",
 ""+blog+"libertadores-href-0.html",""+blog+"libertadores-href-0b.html",""+blog+"libertadores-href-0c.html",
