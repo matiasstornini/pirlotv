23fcb2e8fb271ecad86d84bbdaeb2bb59f1a8ab0
adiccfut <37271131+adiccfut@users.noreply.github.com>
2020-10-03 20:28:36 -0300

    Add files via upload

---
+++ html/dt.js
@@ -0,0 +1,22 @@
+// var dt = [
+// "1601724600","1601733600","1601742600","1601751600"
+// ];
+var dt=[/*ESP*/"1601722800","1601733600","1601742600","1601742600","1601751600",/*MEX*/"1601769600","1601776800","1601830800"];
+var dta = [];
+for (var i = 0; i < 7; i++) {
+
+	var date = new Date(dt[i] * 1000);
+	// Hours part from the timestamp
+	var hours = date.getHours();
+	// Minutes part from the timestamp
+	var minutes = "0" + date.getMinutes();
+	// Seconds part from the timestamp
+	var seconds = "0" + date.getSeconds();
+
+	// Will display time in 10:30:23 format
+	var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+	
+	dta.push(formattedTime);
+
+	console.log(dta[i]);
+}
