c2da66295ce917d8f11569ec60b68982a4b7cc2e
adiccfut <37271131+adiccfut@users.noreply.github.com>
2020-10-03 18:35:08 -0300

    Update enlaces.js

--- html/enlaces.js
+++ html/enlaces.js
@@ -40,7 +40,7 @@ let ALib = [
 ];
 
 let ACol = [
-"<a class='waves-effect waves-light btnoutlinelight outline ahref' href=' "+col[0]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='waves-effect waves-light btnoutlinelight outline ahref' href=' "+col[1]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a>"
+"<a class='waves-effect waves-light btnoutlinelight outline ahref' href=' "+col[0]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <!--<a class='waves-effect waves-light btnoutlinelight outline ahref' href=' "+col[1]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a>-->"
 ];
 let AMex = [
 "<a class='waves-effect waves-light btnoutlinelight outline ahref' href=' "+mex[0]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='waves-effect waves-light btnoutlinelight outline ahref' href=' "+mex[1]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a>"
