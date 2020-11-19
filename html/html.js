d340a97df2bb05891713c0e175e00c4dce37f6e7
adiccfut <37271131+adiccfut@users.noreply.github.com>
2020-11-19 20:47:51 -0300

    Update html.js

--- html/html.js
+++ html/html.js
@@ -1,3 +1,3 @@
-var i = -1;document.getElementById("demo2").innerHTML = "<h4><u>Liga Profesional (Argentina) 19/11/2020</u></h4><h5>"+dta[i=i+1]+"    Atlético Tucumán -  Racing Club </h5> <a class='waves-effect waves-light btnoutlinelight outline ahref' href='"+url+"test/statistics?url=https://m.livesoccertv.com/es/match/3837789/atletico-tucuman-vs-racing-club/ ' style='background-color: #ffffff00;'><b>Estadisticas</b></a> ";
+var i = -1;document.getElementById("demo2").innerHTML = "<h4><u>Liga Profesional (Argentina) 19/11/2020</u></h4><h5>"+dta[i=i+1]+"    Atlético Tucumán -  Racing Club </h5>"+AArg[1]+" <a class='waves-effect waves-light btnoutlinelight outline ahref' href='"+url+"test/statistics?url=https://m.livesoccertv.com/es/match/3837789/atletico-tucuman-vs-racing-club/ ' style='background-color: #ffffff00;'><b>Estadisticas</b></a> ";
 // 
 document.getElementById("prox-off").innerHTML = "listo";
