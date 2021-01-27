fcfb7145590f54118fb77168b21014ccd873b30c
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-01-26 23:18:28 -0300

    Add files via upload

---
+++ html/est.js
@@ -0,0 +1,8 @@
+
+EstEspA = [];
+	var i3 = -1;
+for (var i = 0; i <= 30; i++) {
+	let EstEsp = ["<a class='waves-effect waves-light btnoutlinelight outline ahref'  href='"+Est[i3=i3+1]+"' style='background-color: #ffffff00;'>Estadisticas</a> "];
+	EstEspA.push(EstEsp);
+}
+	console.log(EstEspA);
---
+++ html/match.js
@@ -0,0 +1,16 @@
+let Match=["Sevilla -  Valencia" ,"Almería -  Osasuna", "Rayo Vallecano -  Barcelona","Burnley -  Aston Villa" ,"Chelsea -  Wolverhampton Wanderers", "Brighton &amp; Hove Albion -  Fulham" ,"Everton -  Leicester City","Manchester United -  Sheffield United ","Atalanta -  Lazio", "Juventus -  SPAL"];
+
+let Est=[
+"http://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3894973/sevilla-vs-valencia/",
+"http://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3894975/almeria-vs-osasuna/",
+"http://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3894974/rayo-vallecano-vs-barcelona/",
+"http://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757026/burnley-vs-aston-villa/",
+"http://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757030/chelsea-vs-wolverhampton-wanderers/",
+"http://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757025/brighton-hove-albion-vs-fulham/",
+"http://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757027/everton-vs-leicester-city/",
+"http://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757028/manchester-united-vs-sheffield-united/	",
+"http://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3894026/atalanta-vs-lazio/",
+"http://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887667/juventus-vs-spal/"
+];
+
+console.log(Est);
