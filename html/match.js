9daa96f4f4a6d4c44df2e7c162b6f3f86ff4dfde
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-01-28 00:24:11 -0300

    Add files via upload

--- html/match.js
+++ html/match.js
@@ -1,11 +1,11 @@
-let Match=["Sevilla -  Valencia" ,"Almería -  Osasuna", "Rayo Vallecano -  Barcelona","Burnley -  Aston Villa" ,"Chelsea -  Wolverhampton Wanderers", "Brighton &amp; Hove Albion -  Fulham" ,"Everton -  Leicester City","Manchester United -  Sheffield United ","Atalanta -  Lazio", "Juventus -  SPAL"];
+let Match=["Navalcarnero -  Granada", "Alcoyano -  Athletic Club", "Tottenham Hotspur -  Liverpool", "Napoli -  Spezia", "Grêmio -  Flamengo"];
 
 let Est=[
-"https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3894973/sevilla-vs-valencia/",
-"https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3894975/almeria-vs-osasuna/",
-"https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3894974/rayo-vallecano-vs-barcelona/",
-"https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757026/burnley-vs-aston-villa/",
-"https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757030/chelsea-vs-wolverhampton-wanderers/",
+"https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3894976/navalcarnero-vs-granada/",
+"https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3894977/alcoyano-vs-athletic-club/",
+"https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757033/tottenham-hotspur-vs-liverpool/",
+"http://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3892445/napoli-vs-spezia/",
+"http://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863366/gremio-vs-flamengo/",
 "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757025/brighton-hove-albion-vs-fulham/",
 "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757027/everton-vs-leicester-city/",
 "https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757028/manchester-united-vs-sheffield-united/	",
