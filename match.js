1aa7b55b96083a439a3e408ff4d075b92063d193
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-01-29 00:06:50 -0300

    Add files via upload

---
+++ match.js
@@ -0,0 +1,16 @@
+let Match=["Real Valladolid -  Huesca", "Jaguares de Córdoba -  Alianza Petrolera", "Deportivo Cali -  Envigado"];
+
+let Est=[
+"https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757402/real-valladolid-vs-huesca/",
+"http://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3881129/jaguares-de-cordoba-vs-alianza-petrolera/",
+"http://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3881135/deportivo-cali-vs-envigado/",
+"http://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3892445/napoli-vs-spezia/",
+"http://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863366/gremio-vs-flamengo/",
+"https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757025/brighton-hove-albion-vs-fulham/",
+"https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757027/everton-vs-leicester-city/",
+"https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757028/manchester-united-vs-sheffield-united/	",
+"https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3894026/atalanta-vs-lazio/",
+"https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887667/juventus-vs-spal/"
+];
+
+console.log(Est);
