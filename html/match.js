020ee7d9f7c1c96ada2cc9ff4b6d526e763f788d
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-01-31 23:50:02 -0300

    Add files via upload

--- html/match.js
+++ html/match.js
@@ -1,13 +1,8 @@
-let Match=["  Chelsea -  Burnley ","  Leicester City -  Leeds United ","  West Ham United -  Liverpool ","  Brighton &amp; Hove Albion -  Tottenham Hotspur ", "  Getafe -  Deportivo Alavés ","  Cádiz -  Atlético Madrid ","  Granada -  Celta de Vigo ","  Barcelona -  Athletic Club ", " Napoli - Parma "," Roma - Hellas Verona ", "  Deportes Tolima -  La Equidad ","  Atlético Bucaramanga -  Medellín "];
+let Match=[" Real Betis - Osasuna "," Millonarios - Once Caldas "];
 
 let Est=[
-	'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757037/chelsea-vs-burnley/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757040/leicester-city-vs-leeds-united/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757044/west-ham-united-vs-liverpool/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757036/brighton-hove-albion-vs-tottenham-hotspur/ ',
-
-	'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757397/getafe-vs-deportivo-alaves/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757398/cadiz-vs-atletico-madrid/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757399/granada-vs-celta-de-vigo/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757394/barcelona-vs-athletic-club/ ',
-
-	'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777125/napoli-vs-parma/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777128/roma-vs-hellas-verona/ ',
-
-	'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3881128/deportes-tolima-vs-la-equidad/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3881131/atletico-bucaramanga-vs-medellin/ '
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757395/real-betis-vs-osasuna/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3881136/millonarios-vs-once-caldas/ '
 ];
 
 console.log(Est);
