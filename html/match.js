139860bbef13a4693d3ac984e32e99642ef80b76
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-02-02 13:52:27 -0300

    Add files via upload

--- html/match.js
+++ html/match.js
@@ -1,12 +1,15 @@
 let Match=[" Sheffield United - West Bromwich Albion "," Wolverhampton Wanderers - Arsenal "," Manchester United - Southampton "," Newcastle United - Crystal Palace ",
 " Internazionale - Juventus ",
-" Almería - Sevilla "];
+" Almería - Sevilla ",
+" Envigado - Jaguares de Córdoba "," Boyacá Chicó - Deportivo Cali "," Deportivo Pasto - Atlético Nacional "];
 
 let Est=[
 'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757049/sheffield-united-vs-west-bromwich-albion/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757050/wolverhampton-wanderers-vs-arsenal/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757051/manchester-united-vs-southampton/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757052/newcastle-united-vs-crystal-palace/ ',
 
 'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3898191/internazionale-vs-juventus/ ',
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3899747/almeria-vs-sevilla/ '
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3899747/almeria-vs-sevilla/ ',
+
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3881138/envigado-vs-jaguares-de-cordoba/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3881143/boyaca-chico-vs-deportivo-cali/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3881139/deportivo-pasto-vs-atletico-nacional/ ',
 ];
 
 console.log(Est);
