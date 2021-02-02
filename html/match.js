5bc261cc57ccb478b54f87fd15d4e93e0f23fbcd
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-02-02 00:12:09 -0300

    Add files via upload

--- html/match.js
+++ html/match.js
@@ -1,8 +1,12 @@
-let Match=[" Real Betis - Osasuna "," Millonarios - Once Caldas "];
+let Match=[" Sheffield United - West Bromwich Albion "," Wolverhampton Wanderers - Arsenal "," Manchester United - Southampton "," Newcastle United - Crystal Palace ",
+" Internazionale - Juventus ",
+" Almería - Sevilla "];
 
 let Est=[
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757395/real-betis-vs-osasuna/ ',
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3881136/millonarios-vs-once-caldas/ '
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757049/sheffield-united-vs-west-bromwich-albion/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757050/wolverhampton-wanderers-vs-arsenal/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757051/manchester-united-vs-southampton/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757052/newcastle-united-vs-crystal-palace/ ',
+
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3898191/internazionale-vs-juventus/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3899747/almeria-vs-sevilla/ '
 ];
 
 console.log(Est);
