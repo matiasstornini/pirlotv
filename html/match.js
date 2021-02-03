38eb8b8c39a6c54a6636bfdf63f47576fffed7ea
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-02-02 23:54:40 -0300

    Add files via upload

--- html/match.js
+++ html/match.js
@@ -1,15 +1,17 @@
-let Match=[" Sheffield United - West Bromwich Albion "," Wolverhampton Wanderers - Arsenal "," Manchester United - Southampton "," Newcastle United - Crystal Palace ",
-" Internazionale - Juventus ",
-" Almería - Sevilla ",
-" Envigado - Jaguares de Córdoba "," Boyacá Chicó - Deportivo Cali "," Deportivo Pasto - Atlético Nacional "];
+let Match=[" Burnley - Manchester City "," Fulham - Leicester City "," Leeds United - Everton "," Aston Villa - West Ham United "," Liverpool - Brighton & Hove Albion ",
+" Levante - Villarreal "," Granada - Barcelona ",
+" Napoli - Atalanta ",
+" Grêmio - Santos "];
 
 let Est=[
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757049/sheffield-united-vs-west-bromwich-albion/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757050/wolverhampton-wanderers-vs-arsenal/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757051/manchester-united-vs-southampton/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757052/newcastle-united-vs-crystal-palace/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757046/burnley-vs-manchester-city/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757047/fulham-vs-leicester-city/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757048/leeds-united-vs-everton/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757045/aston-villa-vs-west-ham-united/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757054/liverpool-vs-brighton-hove-albion/ ',
 
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3898191/internazionale-vs-juventus/ ',
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3899747/almeria-vs-sevilla/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3899748/levante-vs-villarreal/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3899749/granada-vs-barcelona/ ',
 
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3881138/envigado-vs-jaguares-de-cordoba/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3881143/boyaca-chico-vs-deportivo-cali/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3881139/deportivo-pasto-vs-atletico-nacional/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3898803/napoli-vs-atalanta/ ',
+
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3892426/gremio-vs-santos/ '
+
 ];
 
 console.log(Est);
