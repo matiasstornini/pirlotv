ac96722cf67f7ccb2ab531cce48534d71f48447e
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-02-03 15:04:50 -0300

    Add files via upload

--- html/match.js
+++ html/match.js
@@ -2,7 +2,8 @@ let Match=[" Burnley - Manchester City "," Fulham - Leicester City "," Leeds Uni
 " Levante - Villarreal "," Granada - Barcelona ",
 " Napoli - Atalanta ",
 " Grêmio - Santos ", 
-" La Equidad - Junior "," Deportivo Pereira - Deportes Tolima "," Alianza Petrolera - Rionegro Águilas "," América de Cali - Atlético Bucaramanga "];
+" La Equidad - Junior "," Deportivo Pereira - Deportes Tolima "," Alianza Petrolera - Rionegro Águilas "," América de Cali - Atlético Bucaramanga ",
+" Peñarol - Nacional "];
 
 let Est=[
 'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757046/burnley-vs-manchester-city/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757047/fulham-vs-leicester-city/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757048/leeds-united-vs-everton/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757045/aston-villa-vs-west-ham-united/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757054/liverpool-vs-brighton-hove-albion/ ',
@@ -12,7 +13,8 @@ let Est=[
 'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3898803/napoli-vs-atalanta/ ',
 
 'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3892426/gremio-vs-santos/ ',
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3881141/la-equidad-vs-junior/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3881140/deportivo-pereira-vs-deportes-tolima/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3881137/alianza-petrolera-vs-rionegro-aguilas/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3881144/america-de-cali-vs-atletico-bucaramanga/ '
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3881141/la-equidad-vs-junior/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3881140/deportivo-pereira-vs-deportes-tolima/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3881137/alianza-petrolera-vs-rionegro-aguilas/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3881144/america-de-cali-vs-atletico-bucaramanga/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3896612/penarol-vs-nacional/ '
 
 ];
 
