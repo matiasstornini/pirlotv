cd1c72f05cba27e33e4b57bc6f7a8803017e553c
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-02-03 13:19:52 -0300

    Add files via upload

--- html/match.js
+++ html/match.js
@@ -1,7 +1,8 @@
 let Match=[" Burnley - Manchester City "," Fulham - Leicester City "," Leeds United - Everton "," Aston Villa - West Ham United "," Liverpool - Brighton & Hove Albion ",
 " Levante - Villarreal "," Granada - Barcelona ",
 " Napoli - Atalanta ",
-" Grêmio - Santos "];
+" Grêmio - Santos ", 
+" La Equidad - Junior "," Deportivo Pereira - Deportes Tolima "," Alianza Petrolera - Rionegro Águilas "," América de Cali - Atlético Bucaramanga "];
 
 let Est=[
 'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757046/burnley-vs-manchester-city/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757047/fulham-vs-leicester-city/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757048/leeds-united-vs-everton/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757045/aston-villa-vs-west-ham-united/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757054/liverpool-vs-brighton-hove-albion/ ',
@@ -10,7 +11,8 @@ let Est=[
 
 'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3898803/napoli-vs-atalanta/ ',
 
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3892426/gremio-vs-santos/ '
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3892426/gremio-vs-santos/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3881141/la-equidad-vs-junior/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3881140/deportivo-pereira-vs-deportes-tolima/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3881137/alianza-petrolera-vs-rionegro-aguilas/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3881144/america-de-cali-vs-atletico-bucaramanga/ '
 
 ];
 
