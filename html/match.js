e7d6c4bbec3bf7346b394de8b736444342603724
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-02-05 23:34:45 -0300

    Add files via upload

--- html/match.js
+++ html/match.js
@@ -1,15 +1,25 @@
 let Match=[
- " Deportivo Alavés - Real Valladolid ",
- " Fiorentina - Internazionale ",
-" Hertha BSC - Bayern München "
+" Aston Villa - Arsenal "," Burnley - Brighton & Hove Albion "," Newcastle United - Southampton "," Fulham - West Ham United "," Manchester United - Everton ",
+
+" Levante - Granada "," Huesca - Real Madrid "," Elche - Villarreal "," Sevilla - Getafe ",
+
+ " Atalanta - Torino "," Sassuolo - Spezia "," Juventus - Roma "," Genoa - Napoli ",
+
+ " Augsburg - Wolfsburg "," Bayer Leverkusen - Stuttgart "," Freiburg - Borussia Dortmund "," Mainz 05 - Union Berlin "," Schalke 04 - RB Leipzig "," Borussia M'gladbach - Köln ",
+
+ " Once Caldas - Envigado "," Junior - Alianza Petrolera "," Atlético Nacional - Boyacá Chicó "
 ];
 
 let Est=[
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757404/deportivo-alaves-vs-real-valladolid/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757055/aston-villa-vs-arsenal/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757056/burnley-vs-brighton-hove-albion/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757061/newcastle-united-vs-southampton/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757057/fulham-vs-west-ham-united/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757060/manchester-united-vs-everton/ ',
 
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777132/fiorentina-vs-internazionale/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757409/levante-vs-granada/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757408/huesca-vs-real-madrid/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757413/elche-vs-villarreal/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757412/sevilla-vs-getafe/ ',
 
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3733941/hertha-bsc-vs-bayern-m-nchen/ '
+ 'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777137/atalanta-vs-torino/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777136/sassuolo-vs-spezia/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777134/juventus-vs-roma/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777133/genoa-vs-napoli/ ',
+
+ 'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3733944/augsburg-vs-wolfsburg/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3733938/bayer-leverkusen-vs-stuttgart/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3733940/freiburg-vs-borussia-dortmund/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3733943/mainz-05-vs-union-berlin/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3733942/schalke-04-vs-rb-leipzig/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3733937/borussia-mgladbach-vs-koln/ ',
+
+ 'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3881150/once-caldas-vs-envigado/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3881148/junior-vs-alianza-petrolera/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3881152/atletico-nacional-vs-boyaca-chico/ '
 ];
 
 console.log(Est);
