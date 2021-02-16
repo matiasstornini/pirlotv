5c62f6caa99506d5a08afac1beb5fe6e0ac404a7
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-02-15 23:57:04 -0300

    Add files via upload

--- match.js
+++ match.js
@@ -1,16 +1,30 @@
-let Match=["Real Valladolid -  Huesca", "Jaguares de Córdoba -  Alianza Petrolera", "Deportivo Cali -  Envigado"];
+let Match=[
+" Barcelona - PSG "," RB Leipzig - Liverpool ",
 
-let Est=[
-"https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757402/real-valladolid-vs-huesca/",
-"http://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3881129/jaguares-de-cordoba-vs-alianza-petrolera/",
-"http://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3881135/deportivo-cali-vs-envigado/",
-"http://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3892445/napoli-vs-spezia/",
-"http://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863366/gremio-vs-flamengo/",
-"https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757025/brighton-hove-albion-vs-fulham/",
-"https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757027/everton-vs-leicester-city/",
-"https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757028/manchester-united-vs-sheffield-united/	",
-"https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3894026/atalanta-vs-lazio/",
-"https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887667/juventus-vs-spal/"
+" Patriotas Boyacá - Alianza Petrolera "," Once Caldas - Atlético Nacional "
 ];
 
+let Est=[
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863373/barcelona-vs-psg/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863371/rb-leipzig-vs-liverpool/ ',
+
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887117/patriotas-boyaca-vs-alianza-petrolera/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887114/once-caldas-vs-atletico-nacional/ '
+];
+
+let Title=[
+"Champions League (Octavos)",
+"Liga BetPlay (Colombia)",
+
+"Premier League (Inglaterra)",
+"LaLiga (España)",
+"Bundesliga (Alemania)",
+"Liga Profesional (Argentina)",
+"Serie A (Italia)",
+"Primera Division (Chile)",
+// "Serie A (Italia)",
+
+"Fifa World Cup",
+
+
+
+];
 console.log(Est);
