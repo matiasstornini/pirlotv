046d401f386f4c8e55b3fb8352cb9e9302f1791a
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-02-16 23:59:28 -0300

    Add files via upload

--- html/match.js
+++ html/match.js
@@ -1,21 +1,29 @@
 let Match=[
-" Barcelona - PSG "," RB Leipzig - Liverpool ",
+" Porto - Juventus "," Sevilla - Borussia Dortmund ",
 
-" Patriotas Boyacá - Alianza Petrolera "," Once Caldas - Atlético Nacional "
+" Burnley - Fulham "," Everton - Manchester City ",
+
+" Levante - Atlético Madrid ",
+
+" Atlético Bucaramanga - Deportivo Pereira "," Rionegro Águilas - Envigado "," Medellín - Boyacá Chicó "," Junior - Deportivo Cali "
 ];
 
 let Est=[
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863373/barcelona-vs-psg/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863371/rb-leipzig-vs-liverpool/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863375/porto-vs-juventus/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863377/sevilla-vs-borussia-dortmund/ ',
 
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887117/patriotas-boyaca-vs-alianza-petrolera/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887114/once-caldas-vs-atletico-nacional/ '
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3906630/burnley-vs-fulham/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3906629/everton-vs-manchester-city/ ',
+
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887091/levante-vs-atletico-madrid/ ',
+
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887113/atletico-bucaramanga-vs-deportivo-pereira/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887115/rionegro-aguilas-vs-envigado/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887116/medellin-vs-boyaca-chico/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887112/junior-vs-deportivo-cali/ '
 ];
 
 let Title=[
 "Champions League (Octavos)",
-"Liga BetPlay (Colombia)",
 
 "Premier League (Inglaterra)",
 "LaLiga (España)",
+"Liga BetPlay (Colombia)",
 "Bundesliga (Alemania)",
 "Liga Profesional (Argentina)",
 "Serie A (Italia)",
