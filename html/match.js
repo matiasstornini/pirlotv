35c137bb0484fc68c3ca8dc45a6e1e63a64f3b34
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-02-19 00:02:18 -0300

    Add files via upload

--- html/match.js
+++ html/match.js
@@ -1,25 +1,35 @@
 let Match=[
-" Dynamo Kyiv - Club Brugge "," Crvena Zvezda - Milan "," Krasnodar - Dinamo Zagreb "," Olympiakos Piraeus - PSV "," Real Sociedad - Manchester United "," Slavia Praha - Leicester City "," Sporting Braga - Roma "," Wolfsberger AC - Tottenham Hotspur "," Young Boys - Bayer Leverkusen "," Antwerp - Rangers "," Benfica - Arsenal "," Granada - Napoli "," Lille - Ajax "," Maccabi Tel Aviv - Shakhtar Donetsk "," Molde - Hoffenheim "," Salzburg - Villarreal ",
+" Wolverhampton Wanderers - Leeds United ",
 
-" Jaguares de Córdoba - La Equidad "," Millonarios - Deportivo Pasto "," América de Cali - Santa Fe "
+" Real Betis - Getafe ",
+
+" Gimnasia La Plata - Talleres Córdoba "," Arsenal - Banfield ",
+
+" Deportes Tolima - Patriotas Boyacá "
 ];
 
 let Est=[
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3862961/dynamo-kyiv-vs-club-brugge/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3862965/crvena-zvezda-vs-milan/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3862971/krasnodar-vs-dinamo-zagreb/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3862975/olympiakos-piraeus-vs-psv/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3862963/real-sociedad-vs-manchester-united/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3862967/slavia-praha-vs-leicester-city/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3862969/sporting-braga-vs-roma/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3862959/wolfsberger-ac-vs-tottenham-hotspur/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3862973/young-boys-vs-bayer-leverkusen/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3862979/antwerp-vs-rangers/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3862977/benfica-vs-arsenal/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3862985/granada-vs-napoli/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3862989/lille-vs-ajax/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3862987/maccabi-tel-aviv-vs-shakhtar-donetsk/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3862983/molde-vs-hoffenheim/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3862981/salzburg-vs-villarreal/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757084/wolverhampton-wanderers-vs-leeds-united/ ',
 
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887111/jaguares-de-cordoba-vs-la-equidad/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887119/millonarios-vs-deportivo-pasto/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887118/america-de-cali-vs-santa-fe/ '
+
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757427/real-betis-vs-getafe/ ',
+
+
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3906656/gimnasia-la-plata-vs-talleres-cordoba/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3906648/arsenal-vs-banfield/ ',
+
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887120/deportes-tolima-vs-patriotas-boyaca/ '
 ];
 
 let Title=[
-"Europa League (Dieciseisavos)",
-"Liga BetPlay (Colombia)",
 
 "Premier League (Inglaterra)",
 "LaLiga (España)",
-"Bundesliga (Alemania)",
 "Liga Profesional (Argentina)",
+"Liga BetPlay (Colombia)",
+"Bundesliga (Alemania)",
 "Serie A (Italia)",
 "Primera Division (Chile)",
+"Europa League (Dieciseisavos)",
 // "Serie A (Italia)",
 
 "Fifa World Cup",
