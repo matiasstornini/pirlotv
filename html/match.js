45664536bd57ebeba61fe3f1f1d8656addfe7544
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-02-22 00:02:08 -0300

    Add files via upload

--- html/match.js
+++ html/match.js
@@ -1,23 +1,23 @@
 let Match=[
-" West Ham United - Tottenham Hotspur "," Aston Villa - Leicester City "," Arsenal - Manchester City "," Manchester United - Newcastle United ",
+" Brighton & Hove Albion - Crystal Palace ",
 
-" Barcelona - Cádiz "," Real Sociedad - Deportivo Alavés "," Huesca - Granada "," Athletic Club - Villarreal ",
+" Osasuna - Sevilla ",
 
-" Parma - Udinese "," Milan - Internazionale "," Atalanta - Napoli "," Benevento - Roma ",
+" Juventus - Crotone ",
 
-" Argentinos Juniors - Platense "," Sarmiento Resistencia - Vélez Sarsfield "," Patronato - Independiente "," Newell's Old Boys - Boca Juniors ",
+" Atlético Tucumán - Central Córdoba SdE "," Huracán - Unión Santa Fe "," Colón - San Lorenzo ",
 
-" La Equidad - Once Caldas "," Deportivo Pasto - Jaguares de Córdoba "," Santa Fe - Medellín "," Atlético Nacional - América de Cali "
+" Deportivo Cali - Alianza Petrolera "
 ];
 
 let Est=[
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757083/west-ham-united-vs-tottenham-hotspur/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757076/aston-villa-vs-leicester-city/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757075/arsenal-vs-manchester-city/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757081/manchester-united-vs-newcastle-united/ ',
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757426/barcelona-vs-cadiz/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757430/real-sociedad-vs-deportivo-alaves/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757428/huesca-vs-granada/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757424/athletic-club-vs-villarreal/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757077/brighton-hove-albion-vs-crystal-palace/ ',
 
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777157/parma-vs-udinese/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777151/milan-vs-internazionale/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777152/atalanta-vs-napoli/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777153/benevento-vs-roma/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757429/osasuna-vs-sevilla/ ',
 
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3906652/argentinos-juniors-vs-platense/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917312/sarmiento-resistencia-vs-velez-sarsfield/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3906655/patronato-vs-independiente/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3906657/newells-old-boys-vs-boca-juniors/ ',
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887124/la-equidad-vs-once-caldas/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887122/deportivo-pasto-vs-jaguares-de-cordoba/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887128/santa-fe-vs-medellin/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887125/atletico-nacional-vs-america-de-cali/ '
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3906659/atletico-tucuman-vs-central-cordoba-sde/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3906653/huracan-vs-union-santa-fe/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3906647/colon-vs-san-lorenzo/ ',
+
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887127/deportivo-cali-vs-alianza-petrolera/ '
 ];
 
 let Title=[
