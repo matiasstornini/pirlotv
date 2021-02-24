96cbe65e969784dbadffdb5ba376dca5788cf936
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-02-23 23:58:22 -0300

    Add files via upload

--- match.js
+++ match.js
@@ -1,24 +1,39 @@
 let Match=[
-" Barcelona - PSG "," RB Leipzig - Liverpool ",
+" Tottenham Hotspur - Wolfsberger AC ",
 
-" Patriotas Boyacá - Alianza Petrolera "," Once Caldas - Atlético Nacional "
+" Barcelona - Elche ",
+
+" Atalanta - Real Madrid "," Borussia M'gladbach - Manchester City ",
+
+" César Vallejo - Caracas "," Royal Pari - Guaraní ",
+
+" Santa Fe - Boyacá Chicó "," Rionegro Águilas - La Equidad "," Atlético Bucaramanga - Atlético Nacional "," América de Cali - Envigado "
 ];
 
 let Est=[
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863373/barcelona-vs-psg/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863371/rb-leipzig-vs-liverpool/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3862960/tottenham-hotspur-vs-wolfsberger-ac/ ',
 
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887117/patriotas-boyaca-vs-alianza-petrolera/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887114/once-caldas-vs-atletico-nacional/ '
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887090/barcelona-vs-elche/ ',
+
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863385/atalanta-vs-real-madrid/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863383/borussia-mgladbach-vs-manchester-city/ ',
+
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3907927/cesar-vallejo-vs-caracas/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3907929/royal-pari-vs-guarani/ ',
+
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887137/santa-fe-vs-boyaca-chico/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887134/rionegro-aguilas-vs-la-equidad/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887132/atletico-bucaramanga-vs-atletico-nacional/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887136/america-de-cali-vs-envigado/ '
 ];
 
 let Title=[
-"Champions League (Octavos)",
-"Liga BetPlay (Colombia)",
 
-"Premier League (Inglaterra)",
+"Europa League (Dieciseisavos)",
 "LaLiga (España)",
-"Bundesliga (Alemania)",
+"Champions League (Octavos)",
+"Conmebol Libertadores ",
+"Liga BetPlay (Colombia)",
 "Liga Profesional (Argentina)",
+"Premier League (Inglaterra)",
+"Copa Argentina",
 "Serie A (Italia)",
+"Bundesliga (Alemania)",
 "Primera Division (Chile)",
 // "Serie A (Italia)",
 
