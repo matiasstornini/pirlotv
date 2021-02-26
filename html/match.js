3852242934469ffca724d37ac07aeaf6097ae4f4
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-02-26 00:06:30 -0300

    Add files via upload

--- html/match.js
+++ html/match.js
@@ -1,28 +1,24 @@
 let Match=[
-" Ajax - Lille "," Arsenal - Benfica "," Hoffenheim - Molde "," Napoli - Granada "," Rangers - Antwerp "," Shakhtar Donetsk - Maccabi Tel Aviv "," Villarreal - Salzburg "," Bayer Leverkusen - Young Boys "," Club Brugge - Dynamo Kyiv "," Dinamo Zagreb - Krasnodar "," Leicester City - Slavia Praha "," Manchester United - Real Sociedad "," Milan - Crvena Zvezda "," PSV - Olympiakos Piraeus "," Roma - Sporting Braga ",
+" Levante - Athletic Club ",
 
-" Once Caldas - Deportivo Pasto "," Jaguares de Córdoba - Deportivo Pereira "," Alianza Petrolera - Deportes Tolima "," Medellín - Deportivo Cali ",
-
-" Huracán - Estudiantes de San Luis "
+" Aldosivi - Arsenal "," Defensa y Justicia - Patronato "," Argentinos Juniors - Vélez Sarsfield "
 ];
 
 let Est=[
-'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3862990/ajax-vs-lille/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3862978/arsenal-vs-benfica/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3862984/hoffenheim-vs-molde/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3862986/napoli-vs-granada/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3862980/rangers-vs-antwerp/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3862988/shakhtar-donetsk-vs-maccabi-tel-aviv/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3862982/villarreal-vs-salzburg/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3862974/bayer-leverkusen-vs-young-boys/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3862962/club-brugge-vs-dynamo-kyiv/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3862972/dinamo-zagreb-vs-krasnodar/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3862968/leicester-city-vs-slavia-praha/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3862964/manchester-united-vs-real-sociedad/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3862966/milan-vs-crvena-zvezda/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3862976/psv-vs-olympiakos-piraeus/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3862970/roma-vs-sporting-braga/ ',
+'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757440/levante-vs-athletic-club/ ',
 
-'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887133/once-caldas-vs-deportivo-pasto/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887129/jaguares-de-cordoba-vs-deportivo-pereira/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887130/alianza-petrolera-vs-deportes-tolima/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887135/medellin-vs-deportivo-cali/ ',
-
-'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3913983/huracan-vs-estudiantes-de-san-luis/ ',
+'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3906663/aldosivi-vs-arsenal/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3906669/defensa-y-justicia-vs-patronato/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3906672/argentinos-juniors-vs-velez-sarsfield/ '
 ];
 
 let Title=[
 
+"LaLiga (España)",
+"Liga Profesional (Argentina)",
 "Europa League (Dieciseisavos)",
 "Liga BetPlay (Colombia)",
 "Copa Argentina",
-"LaLiga (España)",
 "Champions League (Octavos)",
 "Conmebol Libertadores ",
-"Liga Profesional (Argentina)",
 "Premier League (Inglaterra)",
 "Serie A (Italia)",
 "Bundesliga (Alemania)",
