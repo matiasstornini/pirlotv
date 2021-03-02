265c02e0a90d281567f8c5e85f4b2dae7b4317a8
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-03-02 16:13:38 -0300

    Add files via upload

--- html/match.js
+++ html/match.js
@@ -2,6 +2,7 @@ let Match=[
 " Manchester City - Wolverhampton Wanderers ",
 
 " Lazio - Torino "," Juventus - Spezia ",
+ " Borussia M'gladbach - Borussia Dortmund ",
 
 " Universidad Católica - Liverpool ",
 
@@ -9,24 +10,25 @@ let Match=[
 ];
 
 let Est=[
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3922989/manchester-city-vs-wolverhampton-wanderers/ ',
+'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3922989/manchester-city-vs-wolverhampton-wanderers/ ',
 
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777176/lazio-vs-torino/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777175/juventus-vs-spezia/ ',
+'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777176/lazio-vs-torino/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777175/juventus-vs-spezia/ ',
+'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3910209/borussia-mgladbach-vs-borussia-dortmund/ ',
 
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3910148/universidad-catolica-vs-liverpool/ ',
+'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3910148/universidad-catolica-vs-liverpool/ ',
 
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887143/atletico-nacional-vs-alianza-petrolera/ '
+'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887143/atletico-nacional-vs-alianza-petrolera/ '
 ];
 
 let Title=[
 
 "Premier League (Inglaterra)",
 "Serie A (Italia)",
+"Copa de Alemania (Alemania)",
 "Conmebol Libertadores ",
 "Liga BetPlay (Colombia)",
 "LaLiga (España)",
 "Liga Profesional (Argentina)",
-"Bundesliga (Alemania)",
 "Europa League (Dieciseisavos)",
 "Copa Argentina",
 "Champions League (Octavos)",
