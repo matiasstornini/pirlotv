f213042f813bb22db4cd4484bd8396d32b50142c
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-03-02 19:35:54 -0300

    Add files via upload

--- html/match.js
+++ html/match.js
@@ -3,6 +3,8 @@ let Match=[
 
 " Lazio - Torino "," Juventus - Spezia ",
  " Borussia M'gladbach - Borussia Dortmund ",
+" Talleres Córdoba - Atlético Rafaela "," Defensa y Justicia - Estudiantes Caseros ",
+
 
 " Universidad Católica - Liverpool ",
 
@@ -14,6 +16,8 @@ let Est=[
 
 'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777176/lazio-vs-torino/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777175/juventus-vs-spezia/ ',
 'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3910209/borussia-mgladbach-vs-borussia-dortmund/ ',
+'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3925884/talleres-cordoba-vs-atletico-rafaela/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3925885/defensa-y-justicia-vs-estudiantes-caseros/ ',
+
 
 'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3910148/universidad-catolica-vs-liverpool/ ',
 
@@ -25,12 +29,12 @@ let Title=[
 "Premier League (Inglaterra)",
 "Serie A (Italia)",
 "Copa de Alemania (Alemania)",
+"Copa Argentina",
 "Conmebol Libertadores ",
 "Liga BetPlay (Colombia)",
 "LaLiga (España)",
 "Liga Profesional (Argentina)",
 "Europa League (Dieciseisavos)",
-"Copa Argentina",
 "Champions League (Octavos)",
 "Primera Division (Chile)",
 // "Serie A (Italia)",
