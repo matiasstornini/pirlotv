bf6667d8f0f9266d290d1fa4d64778247d8d938f
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-03-03 00:15:16 -0300

    Add files via upload

--- html/match.js
+++ html/match.js
@@ -1,38 +1,40 @@
 let Match=[
-" Manchester City - Wolverhampton Wanderers ",
+" Burnley - Leicester City "," Sheffield United - Aston Villa "," Crystal Palace - Manchester United ",
 
-" Lazio - Torino "," Juventus - Spezia ",
- " Borussia M'gladbach - Borussia Dortmund ",
-" Talleres Córdoba - Atlético Rafaela "," Defensa y Justicia - Estudiantes Caseros ",
+" Barcelona - Sevilla ",
 
+" Sassuolo - Napoli "," Atalanta - Crotone "," Benevento - Hellas Verona "," Cagliari - Bologna "," Fiorentina - Roma "," Genoa - Sampdoria "," Milan - Udinese ",
 
-" Universidad Católica - Liverpool ",
+" Boca Juniors - Claypole ",
 
-" Atlético Nacional - Alianza Petrolera "
+" Caracas - César Vallejo "," Guaraní - Royal Pari ",
+
+" Patriotas Boyacá - América de Cali "
 ];
 
 let Est=[
-'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3922989/manchester-city-vs-wolverhampton-wanderers/ ',
+'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3922990/burnley-vs-leicester-city/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3922991/sheffield-united-vs-aston-villa/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3922992/crystal-palace-vs-manchester-united/ ',
 
-'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777176/lazio-vs-torino/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777175/juventus-vs-spezia/ ',
-'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3910209/borussia-mgladbach-vs-borussia-dortmund/ ',
-'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3925884/talleres-cordoba-vs-atletico-rafaela/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3925885/defensa-y-justicia-vs-estudiantes-caseros/ ',
+'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3907938/barcelona-vs-sevilla/ ',
 
+'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777172/sassuolo-vs-napoli/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777170/atalanta-vs-crotone/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777178/benevento-vs-hellas-verona/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777169/cagliari-vs-bologna/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777173/fiorentina-vs-roma/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777174/genoa-vs-sampdoria/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777177/milan-vs-udinese/ ',
 
-'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3910148/universidad-catolica-vs-liverpool/ ',
+'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3925886/boca-juniors-vs-claypole/ ',
 
-'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887143/atletico-nacional-vs-alianza-petrolera/ '
+'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3907928/caracas-vs-cesar-vallejo/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3907930/guarani-vs-royal-pari/ ',
+
+'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3923716/patriotas-boyaca-vs-america-de-cali/ '
 ];
 
 let Title=[
 
 "Premier League (Inglaterra)",
+"Copa del Rey (España)",
 "Serie A (Italia)",
-"Copa de Alemania (Alemania)",
 "Copa Argentina",
 "Conmebol Libertadores ",
 "Liga BetPlay (Colombia)",
-"LaLiga (España)",
+"Copa de Alemania (Alemania)",
 "Liga Profesional (Argentina)",
 "Europa League (Dieciseisavos)",
 "Champions League (Octavos)",
