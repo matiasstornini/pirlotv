6b6add6fa692f078fb960922f605e05dad6ba5e4
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-03-10 14:08:41 -0300

    Add files via upload

--- html/match.js
+++ html/match.js
@@ -1,15 +1,15 @@
 let Match=[
-" Manchester City - Southampton ",
+" Manchester City - Southampton <b>(Premier League)</b> ",
 
-" Atlético Madrid - Athletic Club ",
+" Atlético Madrid - Athletic Club <b>(LaLiga)</b>",
 
-" Liverpool - RB Leipzig "," PSG - Barcelona ",
+" Liverpool - RB Leipzig <b>(Champions League)</b>"," PSG - Barcelona <b>(Champions League)</b>",
 
-" Platense - Sarmiento ",
+" Platense - Sarmiento <b>(Liga Argentina)</b>",
 
-" Banfield - Club Atlético Güemes "," Atlético Tucumán - Comunicaciones "," Independiente - Villa Mitre ",
+" Banfield - Club Atlético Güemes <b>(Copa Argentina)</b>"," Atlético Tucumán - Comunicaciones <b>(Copa Argentina)</b>"," Independiente - Villa Mitre <b>(Copa Argentina)</b>",
 
-" Universidad Católica - Libertad "," Caracas - Junior "," Grêmio - Ayacucho "," Universidad Chile - San Lorenzo "
+" Universidad Católica - Libertad <b>(Conmebol Libertadores)</b>"," Caracas - Junior <b>(Conmebol Libertadores)</b>"," Grêmio - Ayacucho <b>(Conmebol Libertadores)</b>"," Universidad Chile - San Lorenzo <b>(Conmebol Libertadores)</b>"
 ];
 
 let Est=[
@@ -26,25 +26,28 @@ let Est=[
 'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3934019/universidad-catolica-vs-libertad/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3934020/caracas-vs-junior/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3934018/gremio-vs-ayacucho/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3921773/universidad-chile-vs-san-lorenzo/ '
 ];
 
+let Enl = [
+   AIng[0],AEsp[0],"","","","","","","","","","",
+];
+
 let Title=[
 
-"Premier League (Inglaterra)",
-"LaLiga (España)",
-"Champions League (Octavos)",
-"Liga Profesional (Argentina)",
-"Copa Argentina",
-"Conmebol Libertadores ",
-"Liga BetPlay (Colombia)",
-"Serie A (Italia)",
-"Bundesliga (Alemania)",
+"Premier League <b>(Inglaterra)</b>",
+"LaLiga <b>(España)</b>",
+"Serie A <b>(Italia)</b>",
+"Liga Profesional <b>(Argentina)</b>",
+"Liga BetPlay <b>(Colombia)</b>",
+"Bundesliga <b>(Alemania)</b>",
 "Supercopa Argentina",
-"Europa League (Dieciseisavos)",
-"Primera Division (Chile)",
-// "Serie A (Italia)",
+"Conmebol Libertadores ",
+"Europa League <b>(Dieciseisavos)</b>",
+"Champions League <b>(Octavos)</b>",
+"Primera Division <b>(Chile)</b>",
+// "Serie A <b>(Italia)</b>",
 
 "Fifa World Cup",
 
 
 
 ];
-console.log(Est);
+//console.log<b>(Est)</b>;
