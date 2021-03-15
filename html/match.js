605e02ae28026b241a821c0407419dd124a49832
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-03-14 23:49:27 -0300

    Add files via upload

--- html/match.js
+++ html/match.js
@@ -1,35 +1,28 @@
 let Match=[
-" Southampton - Brighton & Hove Albion <b>(Premier League)</b>"," Leicester City - Sheffield United <b>(Premier League)</b>"," Arsenal - Tottenham Hotspur <b>(Premier League)</b>"," Manchester United - West Ham United <b>(Premier League)</b>",
+" Wolverhampton Wanderers - Liverpool <b>(Premier League)</b>",
 
-" Celta de Vigo - Athletic Club <b>(LaLiga)</b>"," Granada - Real Sociedad <b>(LaLiga)</b>"," Eibar - Villarreal <b>(LaLiga)</b>"," Sevilla - Real Betis <b>(LaLiga)</b>",
+" Barcelona - Huesca <b>(LaLiga)</b>",
 
-" Bologna - Sampdoria <b>(Serie A)</b>"," Parma - Roma <b>(Serie A)</b>"," Torino - Internazionale <b>(Serie A)</b>"," Cagliari - Juventus <b>(Serie A)</b>"," Milan - Napoli <b>(Serie A)</b>",
+" Atlético Tucumán - Patronato <b>(Liga Profesional)</b>"," Rosario Central - Arsenal <b>(Liga Profesional)</b>"," Independiente - Sarmiento <b>(Liga Profesional)</b>",
 
-" Boca Juniors - River Plate  <b>(Liga Profesional)</b>"," Banfield - San Lorenzo <b>(Liga Profesional)</b>",
-
-" Rionegro Águilas - Medellín <b>(Liga BetPlay)</b>"," Atlético Nacional - Junior <b>(Liga BetPlay)</b>",
-
-
+" Atlético Bucaramanga - Once Caldas <b>(Liga BetPlay)</b>"
 ];
 
 let Est=[
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757113/southampton-vs-brighton-hove-albion/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757110/leicester-city-vs-sheffield-united/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757105/arsenal-vs-tottenham-hotspur/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757111/manchester-united-vs-west-ham-united/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757114/wolverhampton-wanderers-vs-liverpool/ ',
 
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757456/celta-de-vigo-vs-athletic-club/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757459/granada-vs-real-sociedad/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757457/eibar-vs-villarreal/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757463/sevilla-vs-real-betis/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757455/barcelona-vs-huesca/ ',
 
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777195/bologna-vs-sampdoria/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777194/parma-vs-roma/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777191/torino-vs-internazionale/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777192/cagliari-vs-juventus/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777193/milan-vs-napoli/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3912878/atletico-tucuman-vs-patronato/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3912870/rosario-central-vs-arsenal/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3924456/independiente-vs-sarmiento/ ',
 
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3912880/boca-juniors-vs-river-plate/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3912873/banfield-vs-san-lorenzo/ ',
-
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887159/rionegro-aguilas-vs-medellin/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887162/atletico-nacional-vs-junior/ '
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887157/atletico-bucaramanga-vs-once-caldas/ ',
 ];
 
 let Enl = [
-    "","","","",
-    "","","","",
-    "","","","","",
-    "",AArg[0],
-    ACol[0],ACol[0]
+    "",
+    "",
+    "","","",
+    ""
 ];
 
 let Title=[
