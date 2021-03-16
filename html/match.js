ed027773050d63bb0a1fcc7a29d4b2c10c4b18ee
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-03-15 23:54:52 -0300

    Add files via upload

--- html/match.js
+++ html/match.js
@@ -1,42 +1,45 @@
 let Match=[
-" Wolverhampton Wanderers - Liverpool <b>(Premier League)</b>",
+" Manchester City - Borussia M'gladbach "," Real Madrid - Atalanta ",
 
-" Barcelona - Huesca <b>(LaLiga)</b>",
+" Bolívar - Wanderers "," Deportivo Lara - Santos "," Ayacucho - Grêmio "," Independiente del Valle - Unión Española ",
 
-" Atlético Tucumán - Patronato <b>(Liga Profesional)</b>"," Rosario Central - Arsenal <b>(Liga Profesional)</b>"," Independiente - Sarmiento <b>(Liga Profesional)</b>",
+" 12 de Octubre - Nacional Asunción "," Macará - Emelec "," Antofagasta - Huachipato "," Deportes Tolima - Deportivo Cali ",
 
-" Atlético Bucaramanga - Once Caldas <b>(Liga BetPlay)</b>"
+" Deportivo Pereira - Boyacá Chicó ",
+
 ];
 
 let Est=[
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757114/wolverhampton-wanderers-vs-liverpool/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863384/manchester-city-vs-borussia-mgladbach/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863386/real-madrid-vs-atalanta/ ',
 
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757455/barcelona-vs-huesca/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3925705/bolivar-vs-wanderers/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3927493/deportivo-lara-vs-santos/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3935093/ayacucho-vs-gremio/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3921772/independiente-del-valle-vs-union-espanola/ ',
 
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3912878/atletico-tucuman-vs-patronato/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3912870/rosario-central-vs-arsenal/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3924456/independiente-vs-sarmiento/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917285/12-de-octubre-vs-nacional-asuncion/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3930381/macara-vs-emelec/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3922997/antofagasta-vs-huachipato/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917287/deportes-tolima-vs-deportivo-cali/ ',
 
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887157/atletico-bucaramanga-vs-once-caldas/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887160/deportivo-pereira-vs-boyaca-chico/ ',
+
 ];
 
 let Enl = [
-    "",
-    "",
-    "","",AArg[0],
-    ACol[0]
+    "","",
+    "","","","",
+    "","","","",
+    ""
 ];
 
 let Title=[
 //temoralmente obsoleto
+"Champions League <b>(Octavos)</b>","Champions League <b>(Octavos)</b>",
+"Conmebol Libertadores ","Conmebol Libertadores ","Conmebol Libertadores ","Conmebol Libertadores ",
+"Conmebol Sudamericana ","Conmebol Sudamericana ","Conmebol Sudamericana ","Conmebol Sudamericana ",
+"Liga BetPlay <b>(Colombia)</b>",
 "Premier League <b>(Inglaterra)</b>",
-"LaLiga <b><b>(España)</b>",
 "Serie A <b><b>(Italia)</b>",
+"LaLiga <b><b>(España)</b>",
 "Liga Profesional <b><b>(Argentina)</b>",
-"Liga BetPlay <b><b>(Colombia)</b>",
 "Bundesliga <b><b>(Alemania)</b>",
 "Supercopa Argentina",
-"Conmebol Libertadores ",
 "Europa League <b>(Dieciseisavos)</b>",
-"Champions League <b>(Octavos)</b>",
 "Primera Division <b>(Chile)</b>",
 // "Serie A <b>(Italia)</b>",
 
