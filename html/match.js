277255294fa64053155e9de6f62dd5eded7d89f8
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-03-16 23:59:28 -0300

    Add files via upload

--- html/match.js
+++ html/match.js
@@ -1,44 +1,50 @@
 let Match=[
-" Manchester City - Borussia M'gladbach "," Real Madrid - Atalanta ",
+" Sevilla - Elche ",
 
-" Bolívar - Wanderers "," Deportivo Lara - Santos "," Ayacucho - Grêmio "," Independiente del Valle - Unión Española ",
+" Bayern München - Lazio "," Chelsea - Atlético Madrid ",
 
-" 12 de Octubre - Nacional Asunción "," Macará - Emelec "," Antofagasta - Huachipato "," Deportes Tolima - Deportivo Cali ",
+" Libertad - Universidad Católica "," Junior - Caracas "," San Lorenzo - Universidad Chile ",
 
-" Deportivo Pereira - Boyacá Chicó ",
+" Guabirá - Nacional Potosí "," Aragua - Mineros de Guayana "," UTC Cajamarca - Sport Huancayo "," Wilstermann - Atlético Palmaflor ",
+
+" Deportivo Laferrere - San Telmo "," Colón - Argentinos Juniors "," Racing Club - Sportivo Belgrano ",
+
 
 ];
 
 let Est=[
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863384/manchester-city-vs-borussia-mgladbach/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863386/real-madrid-vs-atalanta/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887092/sevilla-vs-elche/ ',
 
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3925705/bolivar-vs-wanderers/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3927493/deportivo-lara-vs-santos/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3935093/ayacucho-vs-gremio/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3921772/independiente-del-valle-vs-union-espanola/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863380/bayern-m-nchen-vs-lazio/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863382/chelsea-vs-atletico-madrid/ ',
 
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917285/12-de-octubre-vs-nacional-asuncion/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3930381/macara-vs-emelec/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3922997/antofagasta-vs-huachipato/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917287/deportes-tolima-vs-deportivo-cali/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3935094/libertad-vs-universidad-catolica/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3935095/junior-vs-caracas/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3921774/san-lorenzo-vs-universidad-chile/ ',
 
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887160/deportivo-pereira-vs-boyaca-chico/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917289/guabira-vs-nacional-potosi/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917291/aragua-vs-mineros-de-guayana/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917293/utc-cajamarca-vs-sport-huancayo/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917295/wilstermann-vs-atletico-palmaflor/ ',
+
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3936963/deportivo-laferrere-vs-san-telmo/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3936962/colon-vs-argentinos-juniors/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3936966/racing-club-vs-sportivo-belgrano/ '
 
 ];
 
 let Enl = [
+    "",
     "","",
+    "","","",
     "","","","",
-    "","","","",
-    ACol[0]
+    "","",""
 ];
 
 let Title=[
 //temoralmente obsoleto
+"LaLiga <b><b>(España)</b>",
 "Champions League <b>(Octavos)</b>","Champions League <b>(Octavos)</b>",
-"Conmebol Libertadores ","Conmebol Libertadores ","Conmebol Libertadores ","Conmebol Libertadores ",
+"Conmebol Libertadores ","Conmebol Libertadores ","Conmebol Libertadores ",
 "Conmebol Sudamericana ","Conmebol Sudamericana ","Conmebol Sudamericana ","Conmebol Sudamericana ",
+"Copa Argentina","Copa Argentina","Copa Argentina",
 "Liga BetPlay <b>(Colombia)</b>",
 "Premier League <b>(Inglaterra)</b>",
 "Serie A <b><b>(Italia)</b>",
-"LaLiga <b><b>(España)</b>",
-"Liga Profesional <b><b>(Argentina)</b>",
+"Liga Profesional <b><b>(Argentina)</b>","Liga Profesional <b><b>(Argentina)</b>","Liga Profesional <b><b>(Argentina)</b>",
 "Bundesliga <b><b>(Alemania)</b>",
-"Supercopa Argentina",
 "Europa League <b>(Dieciseisavos)</b>",
 "Primera Division <b>(Chile)</b>",
 // "Serie A <b>(Italia)</b>",
