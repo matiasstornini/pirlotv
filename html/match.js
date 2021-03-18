e309ad94b95964ecc1f3f0983b872d9e3e98c650
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-03-17 23:43:22 -0300

    Add files via upload

--- html/match.js
+++ html/match.js
@@ -1,50 +1,40 @@
 let Match=[
-" Sevilla - Elche ",
+" Arsenal - Olympiakos Piraeus "," Dinamo Zagreb - Tottenham Hotspur "," Molde - Granada "," Shakhtar Donetsk - Roma "," Milan - Manchester United "," Rangers - Slavia Praha "," Villarreal - Dynamo Kyiv "," Young Boys - Ajax ",
 
-" Bayern München - Lazio "," Chelsea - Atlético Madrid ",
+" Atlético Nacional - Guaraní ",
 
-" Libertad - Universidad Católica "," Junior - Caracas "," San Lorenzo - Universidad Chile ",
+" Aucas - Guayaquil City "," Guaireña - River Plate "," Metropolitanos - Academia Puerto Cabello "," Carlos Manucci - Melgar "," Cobresal - Palestino "," La Equidad - Deportivo Pasto ",
 
-" Guabirá - Nacional Potosí "," Aragua - Mineros de Guayana "," UTC Cajamarca - Sport Huancayo "," Wilstermann - Atlético Palmaflor ",
-
-" Deportivo Laferrere - San Telmo "," Colón - Argentinos Juniors "," Racing Club - Sportivo Belgrano ",
 
 
 ];
 
 let Est=[
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887092/sevilla-vs-elche/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3929601/arsenal-vs-olympiakos-piraeus/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3929603/dinamo-zagreb-vs-tottenham-hotspur/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3929604/molde-vs-granada/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3929600/shakhtar-donetsk-vs-roma/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3929597/milan-vs-manchester-united/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3929599/rangers-vs-slavia-praha/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3929596/villarreal-vs-dynamo-kyiv/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3929594/young-boys-vs-ajax/ ',
 
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863380/bayern-m-nchen-vs-lazio/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863382/chelsea-vs-atletico-madrid/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3935096/atletico-nacional-vs-guarani/ ',
 
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3935094/libertad-vs-universidad-catolica/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3935095/junior-vs-caracas/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3921774/san-lorenzo-vs-universidad-chile/ ',
-
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917289/guabira-vs-nacional-potosi/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917291/aragua-vs-mineros-de-guayana/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917293/utc-cajamarca-vs-sport-huancayo/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917295/wilstermann-vs-atletico-palmaflor/ ',
-
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3936963/deportivo-laferrere-vs-san-telmo/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3936962/colon-vs-argentinos-juniors/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3936966/racing-club-vs-sportivo-belgrano/ '
-
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917301/aucas-vs-guayaquil-city/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917297/guairena-vs-river-plate/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917299/metropolitanos-vs-academia-puerto-cabello/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917305/carlos-manucci-vs-melgar/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3922998/cobresal-vs-palestino/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917303/la-equidad-vs-deportivo-pasto/ '
 ];
 
 let Enl = [
+    "","","","","","","","",
     "",
-    "","",
-    "",AEsp[4] ,AEsp[3],
-    "","","","",
-    "","",AMex[3]
+    "","","","","","",
 ];
 
 let Title=[
+"Europa League <b>(Dieciseisavos)</b>","Europa League <b>(Dieciseisavos)</b>","Europa League <b>(Dieciseisavos)</b>","Europa League <b>(Dieciseisavos)</b>","Europa League <b>(Dieciseisavos)</b>","Europa League <b>(Dieciseisavos)</b>","Europa League <b>(Dieciseisavos)</b>","Europa League <b>(Dieciseisavos)</b>",
+"Conmebol Libertadores ",
+"Conmebol Sudamericana ","Conmebol Sudamericana ","Conmebol Sudamericana ","Conmebol Sudamericana ","Conmebol Sudamericana ","Conmebol Sudamericana ","Conmebol Sudamericana ",
 "LaLiga <b><b>(España)</b>",
 "Champions League <b>(Octavos)</b>","Champions League <b>(Octavos)</b>",
-"Conmebol Libertadores ","Conmebol Libertadores ","Conmebol Libertadores ",
-"Conmebol Sudamericana ","Conmebol Sudamericana ","Conmebol Sudamericana ","Conmebol Sudamericana ",
 "Copa Argentina","Copa Argentina","Copa Argentina",
 "Liga BetPlay <b>(Colombia)</b>",
 "Premier League <b>(Inglaterra)</b>",
 "Serie A <b><b>(Italia)</b>",
 "Liga Profesional <b><b>(Argentina)</b>","Liga Profesional <b><b>(Argentina)</b>","Liga Profesional <b><b>(Argentina)</b>",
 "Bundesliga <b><b>(Alemania)</b>",
-"Europa League <b>(Dieciseisavos)</b>",
 "Primera Division <b>(Chile)</b>",
 // "Serie A <b>(Italia)</b>",
 
