be44486ed700ffa753b3662fecaa51dc3eef18ea
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-03-18 23:52:41 -0300

    Add files via upload

--- html/match.js
+++ html/match.js
@@ -1,39 +1,42 @@
 let Match=[
-" Arsenal - Olympiakos Piraeus "," Dinamo Zagreb - Tottenham Hotspur "," Molde - Granada "," Shakhtar Donetsk - Roma "," Milan - Manchester United "," Rangers - Slavia Praha "," Villarreal - Dynamo Kyiv "," Young Boys - Ajax ",
+" Fulham - Leeds United ",
 
-" Atlético Nacional - Guaraní ",
+" Real Betis - Levante ",
 
-" Aucas - Guayaquil City "," Guaireña - River Plate "," Metropolitanos - Academia Puerto Cabello "," Carlos Manucci - Melgar "," Cobresal - Palestino "," La Equidad - Deportivo Pasto ",
+" Newell's Old Boys - Unión Santa Fe "," Central Córdoba SdE - Estudiantes ",
 
-
-
+" Jaguares de Córdoba - Rionegro Águilas "
 ];
 
 let Est=[
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3929601/arsenal-vs-olympiakos-piraeus/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3929603/dinamo-zagreb-vs-tottenham-hotspur/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3929604/molde-vs-granada/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3929600/shakhtar-donetsk-vs-roma/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3929597/milan-vs-manchester-united/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3929599/rangers-vs-slavia-praha/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3929596/villarreal-vs-dynamo-kyiv/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3929594/young-boys-vs-ajax/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757118/fulham-vs-leeds-united/ ',
 
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3935096/atletico-nacional-vs-guarani/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757466/real-betis-vs-levante/ ',
 
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917301/aucas-vs-guayaquil-city/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917297/guairena-vs-river-plate/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917299/metropolitanos-vs-academia-puerto-cabello/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917305/carlos-manucci-vs-melgar/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3922998/cobresal-vs-palestino/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917303/la-equidad-vs-deportivo-pasto/ '
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917324/newells-old-boys-vs-union-santa-fe/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917317/central-cordoba-sde-vs-estudiantes/ ',
+
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887165/jaguares-de-cordoba-vs-rionegro-aguilas/ '
 ];
 
 let Enl = [
-    "","","","","","","","",
-    ALib[0],
-    "","","",AEsp[3],AEsp[4],AIng[3],
+    "",
+    "",
+    "","",
+    "",
+    "","","",
 ];
 
 let Title=[
+"Premier League <b>(Inglaterra)</b>",
+"LaLiga <b><b>(España)</b>",
+"Liga Profesional <b><b>(Argentina)</b>","Liga Profesional <b><b>(Argentina)</b>",
+"Liga BetPlay <b>(Colombia)</b>",
 "Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>",
 "Conmebol Libertadores",
 "Conmebol Sudamericana","Conmebol Sudamericana","Conmebol Sudamericana","Conmebol Sudamericana","Conmebol Sudamericana","Conmebol Sudamericana","Conmebol Sudamericana",
-"LaLiga <b><b>(España)</b>",
 "Champions League <b>(Octavos)</b>","Champions League <b>(Octavos)</b>",
 "Copa Argentina","Copa Argentina","Copa Argentina",
-"Liga BetPlay <b>(Colombia)</b>",
-"Premier League <b>(Inglaterra)</b>",
 "Serie A <b><b>(Italia)</b>",
-"Liga Profesional <b><b>(Argentina)</b>","Liga Profesional <b><b>(Argentina)</b>","Liga Profesional <b><b>(Argentina)</b>",
 "Bundesliga <b><b>(Alemania)</b>",
 "Primera Division <b>(Chile)</b>",
 // "Serie A <b>(Italia)</b>",
