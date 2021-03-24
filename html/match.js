b9addda46049e24b6df07fce64c0520fb55b91d9
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-03-24 12:34:16 -0300

    Update match.js

--- html/match.js
+++ html/match.js
@@ -17,6 +17,7 @@ let Est=[
 'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863118/france-vs-ukraine/ ',
 'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863115/portugal-vs-azerbaijan/ ',
     
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3906671/atletico-tucuman-vs-huracan/ ',
 'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3952557/boca-juniors-vs-defensores-de-belgrano/ ',
 
 'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887181/america-de-cali-vs-medellin/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887174/deportes-tolima-vs-junior/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887179/atletico-nacional-vs-jaguares-de-cordoba/ ',
