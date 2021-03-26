5122945f80e00ab4746eef0058ea063fc2f27187
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-03-25 23:58:48 -0300

    Add files via upload

--- html/match.js
+++ html/match.js
@@ -1,55 +1,41 @@
-    let Match=[
-"España - Grecia "," Italia - Irlanda Norte "," Inglaterra - San Marino"," Alemania - Islandia",
+let Match=[
+" Lanús - Patronato "," Unión Santa Fe - Sarmiento ",
+];
 
-" San Lorenzo - Defensa y Justicia ",
+let Est=[
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917340/lanus-vs-patronato/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3924458/union-santa-fe-vs-sarmiento/ ',
+];
 
-" Deportivo Pasto - Patriotas Boyacá "," La Equidad - Alianza Petrolera "," Atlético Bucaramanga - Deportivo Cali "," Rionegro Águilas - Millonarios ",
+let Enl = [
+    "",""
+];
 
+let Title=[
+"Clasificatorio para la Copa del Mundo","Clasificatorio para la Copa del Mundo","Clasificatorio para la Copa del Mundo","Clasificatorio para la Copa del Mundo",
+"Copa Argentina",/*"Copa Argentina","Copa Argentina",*/
+"Liga BetPlay <b>(Colombia)</b>","Liga BetPlay <b>(Colombia)</b>","Liga BetPlay <b>(Colombia)</b>","Liga BetPlay <b>(Colombia)</b>",
 
+"Liga Profesional <b><b>(Argentina)</b>",/*"Liga Profesional <b><b>(Argentina)</b>",*/
+"FA CUP <b>(Inglaterra)</b>","FA CUP <b>(Inglaterra)</b>",
+"Premier League <b>(Inglaterra)</b>","Premier League <b>(Inglaterra)</b>",
 
-    ];
+"LaLiga <b><b>(España)</b>","LaLiga <b><b>(España)</b>","LaLiga <b><b>(España)</b>","LaLiga <b><b>(España)</b>","LaLiga <b><b>(España)</b>",
 
-    let Est=[
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863127/spain-vs-greece/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863130/italy-vs-northern-ireland/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863135/england-vs-san-marino/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863137/germany-vs-iceland/ ',
-
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3952558/san-lorenzo-vs-defensa-y-justicia/ ',
-        
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887176/deportivo-pasto-vs-patriotas-boyaca/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887178/la-equidad-vs-alianza-petrolera/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887175/atletico-bucaramanga-vs-deportivo-cali/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887177/rionegro-aguilas-vs-millonarios/ ',
-
-    ];
-
-    let Enl = [
-        "","","","",
-        "",
-        "","",ACol[0],ACol[0]
-    ];
-
-    let Title=[
-    "Clasificatorio para la Copa del Mundo","Clasificatorio para la Copa del Mundo","Clasificatorio para la Copa del Mundo","Clasificatorio para la Copa del Mundo",
-    "Copa Argentina",/*"Copa Argentina","Copa Argentina",*/
-    "Liga BetPlay <b>(Colombia)</b>","Liga BetPlay <b>(Colombia)</b>","Liga BetPlay <b>(Colombia)</b>","Liga BetPlay <b>(Colombia)</b>",
-
-    "Liga Profesional <b><b>(Argentina)</b>",/*"Liga Profesional <b><b>(Argentina)</b>",*/
-    "FA CUP <b>(Inglaterra)</b>","FA CUP <b>(Inglaterra)</b>",
-    "Premier League <b>(Inglaterra)</b>","Premier League <b>(Inglaterra)</b>",
-
-    "LaLiga <b><b>(España)</b>","LaLiga <b><b>(España)</b>","LaLiga <b><b>(España)</b>","LaLiga <b><b>(España)</b>","LaLiga <b><b>(España)</b>",
-
-    "Serie A <b><b>(Italia)</b>","Serie A <b><b>(Italia)</b>","Serie A <b><b>(Italia)</b>","Serie A <b><b>(Italia)</b>","Serie A <b><b>(Italia)</b>","Serie A <b><b>(Italia)</b>",
+"Serie A <b><b>(Italia)</b>","Serie A <b><b>(Italia)</b>","Serie A <b><b>(Italia)</b>","Serie A <b><b>(Italia)</b>","Serie A <b><b>(Italia)</b>","Serie A <b><b>(Italia)</b>",
 
 
 
-    "Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>",
-    "Conmebol Libertadores",
-    "Conmebol Sudamericana","Conmebol Sudamericana","Conmebol Sudamericana","Conmebol Sudamericana","Conmebol Sudamericana","Conmebol Sudamericana","Conmebol Sudamericana",
-    "Champions League <b>(Octavos)</b>","Champions League <b>(Octavos)</b>",
-    "Bundesliga <b><b>(Alemania)</b>","Bundesliga <b><b>(Alemania)</b>","Bundesliga <b><b>(Alemania)</b>","Bundesliga <b><b>(Alemania)</b>","Bundesliga <b><b>(Alemania)</b>",
-    "Primera Division <b>(Chile)</b>",
-    // "Serie A <b>(Italia)</b>",
+"Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>",
+"Conmebol Libertadores",
+"Conmebol Sudamericana","Conmebol Sudamericana","Conmebol Sudamericana","Conmebol Sudamericana","Conmebol Sudamericana","Conmebol Sudamericana","Conmebol Sudamericana",
+"Champions League <b>(Octavos)</b>","Champions League <b>(Octavos)</b>",
+"Bundesliga <b><b>(Alemania)</b>","Bundesliga <b><b>(Alemania)</b>","Bundesliga <b><b>(Alemania)</b>","Bundesliga <b><b>(Alemania)</b>","Bundesliga <b><b>(Alemania)</b>",
+"Primera Division <b>(Chile)</b>",
+// "Serie A <b>(Italia)</b>",
 
 
 
 
 
-    ];
-    //console.log<b>(Est)</b></b>;
+];
+//console.log<b>(Est)</b></b>;
