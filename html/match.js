b8a4db64b59088a97782655d2d8f4cd9706c6b17
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-03-27 23:59:41 -0300

    Add files via upload

--- html/match.js
+++ html/match.js
@@ -1,29 +1,29 @@
 let Match=[
-"Serbia - Portugal ","Rep. Checa - Belgica",
+"Kazajistan - Francia"," Georgia - España "," Albania - Inglaterra"," Bulgaria - Italia"," Rumania - Alemania",
 
-" Platense - Colón "," Aldosivi - Banfield "," Argentinos Juniors - Arsenal "," Rosario Central - Central Córdoba SdE ",
+" Talleres Córdoba - Godoy Cruz "," Defensa y Justicia - Vélez Sarsfield "," River Plate - Racing Club "," Independiente - Boca Juniors ",
 
-" Envigado - Deportes Tolima "," Once Caldas - Boyacá Chicó "," Junior - Santa Fe "," Medellín - Atlético Nacional ",
+" Deportivo Pereira - La Equidad "," Alianza Petrolera - América de Cali "," Patriotas Boyacá - Rionegro Águilas "," Millonarios - Atlético Bucaramanga ",
+
 
 ];
 
 let Est=[
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863145/serbia-vs-portugal/','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863147/czech-republic-vs-belgium/',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863150/kazakhstan-vs-france/','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863151/georgia-vs-spain/','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863156/bulgaria-vs-italy/','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863164/romania-vs-germany/',
     
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917331/platense-vs-colon/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917334/aldosivi-vs-banfield/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917330/argentinos-juniors-vs-arsenal/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917332/rosario-central-vs-central-cordoba-sde/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917341/talleres-cordoba-vs-godoy-cruz/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917336/defensa-y-justicia-vs-velez-sarsfield/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917329/river-plate-vs-racing-club/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917335/independiente-vs-boca-juniors/ ',
 
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887186/envigado-vs-deportes-tolima/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887185/once-caldas-vs-boyaca-chico/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887184/junior-vs-santa-fe/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887188/medellin-vs-atletico-nacional/ ',
-
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887187/deportivo-pereira-vs-la-equidad/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887183/alianza-petrolera-vs-america-de-cali/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887189/patriotas-boyaca-vs-rionegro-aguilas/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887191/millonarios-vs-atletico-bucaramanga/ ',
 ];
 
 let Enl = [
-    "","",
-    "","","",AArg[0],
-    "","","",ACol[0]
+    "","","","","",
+    "","","","",
+    "","","","",
 ];
 
 let Title=[
-"Clasificatorio para la Copa del Mundo","Clasificatorio para la Copa del Mundo",
+"Clasificatorio para la Copa del Mundo","Clasificatorio para la Copa del Mundo","Clasificatorio para la Copa del Mundo","Clasificatorio para la Copa del Mundo","Clasificatorio para la Copa del Mundo",
 "Liga Profesional <b><b>(Argentina)</b>","Liga Profesional <b><b>(Argentina)</b>","Liga Profesional <b><b>(Argentina)</b>","Liga Profesional <b><b>(Argentina)</b>",
 "Liga BetPlay <b>(Colombia)</b>","Liga BetPlay <b>(Colombia)</b>","Liga BetPlay <b>(Colombia)</b>","Liga BetPlay <b>(Colombia)</b>","Liga BetPlay <b>(Colombia)</b>",
 
