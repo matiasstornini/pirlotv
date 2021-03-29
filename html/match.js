2bdd517b90d14be4e7f4ca0dade561f9e9414199
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-03-29 00:08:00 -0300

    Add files via upload

--- html/match.js
+++ html/match.js
@@ -1,30 +1,25 @@
 let Match=[
-"Kazajistan - Francia"," Georgia - España "," Albania - Inglaterra"," Bulgaria - Italia"," Rumania - Alemania",
+" Estudiantes - San Lorenzo "," Atlético Tucumán - Newell's Old Boys "," Huracán - Gimnasia La Plata ",
 
-" Talleres Córdoba - Godoy Cruz "," Defensa y Justicia - Vélez Sarsfield "," River Plate - Racing Club "," Independiente - Boca Juniors ",
-
-" Deportivo Pereira - La Equidad "," Alianza Petrolera - América de Cali "," Patriotas Boyacá - Rionegro Águilas "," Millonarios - Atlético Bucaramanga ",
+" Deportivo Cali - Deportivo Pasto ",
 
 
 ];
 
 let Est=[
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863150/kazakhstan-vs-france/','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863151/georgia-vs-spain/','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863156/bulgaria-vs-italy/','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863164/romania-vs-germany/','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863164/romania-vs-germany/',
-    
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917341/talleres-cordoba-vs-godoy-cruz/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917336/defensa-y-justicia-vs-velez-sarsfield/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917329/river-plate-vs-racing-club/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917335/independiente-vs-boca-juniors/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917333/estudiantes-vs-san-lorenzo/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917338/atletico-tucuman-vs-newells-old-boys/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917339/huracan-vs-gimnasia-la-plata/ ',
 
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887187/deportivo-pereira-vs-la-equidad/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887183/alianza-petrolera-vs-america-de-cali/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887189/patriotas-boyaca-vs-rionegro-aguilas/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887191/millonarios-vs-atletico-bucaramanga/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887190/deportivo-cali-vs-deportivo-pasto/ ',
 ];
 
 let Enl = [
-    "","","","","",
-    "","","",AArg[0]+" "+AMex[1],
-    "","",ACol[0],ACol[0],
+    "","","",
+    "",
+    "",
 ];
 
 let Title=[
-"Clasificatorio para la Copa del Mundo","Clasificatorio para la Copa del Mundo","Clasificatorio para la Copa del Mundo","Clasificatorio para la Copa del Mundo","Clasificatorio para la Copa del Mundo",
-"Liga Profesional <b><b>(Argentina)</b>","Liga Profesional <b><b>(Argentina)</b>","Liga Profesional <b><b>(Argentina)</b>","Liga Profesional <b><b>(Argentina)</b>",
+"Liga Profesional <b><b>(Argentina)</b>","Liga Profesional <b><b>(Argentina)</b>","Liga Profesional <b><b>(Argentina)</b>",
 "Liga BetPlay <b>(Colombia)</b>","Liga BetPlay <b>(Colombia)</b>","Liga BetPlay <b>(Colombia)</b>","Liga BetPlay <b>(Colombia)</b>","Liga BetPlay <b>(Colombia)</b>",
 
 "Copa Argentina",/*"Copa Argentina","Copa Argentina",*/
@@ -36,6 +31,7 @@ let Title=[
 "Serie A <b><b>(Italia)</b>","Serie A <b><b>(Italia)</b>","Serie A <b><b>(Italia)</b>","Serie A <b><b>(Italia)</b>","Serie A <b><b>(Italia)</b>","Serie A <b><b>(Italia)</b>",
 
 
+"Clasificatorio para la Copa del Mundo","Clasificatorio para la Copa del Mundo","Clasificatorio para la Copa del Mundo","Clasificatorio para la Copa del Mundo","Clasificatorio para la Copa del Mundo",
 
 "Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>",
 "Conmebol Libertadores",
