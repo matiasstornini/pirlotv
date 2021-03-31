34b0d5aa1e8e0cd7f371ae890cf92c2475af96d4
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-03-31 12:01:37 -0300

    Add files via upload

--- html/match.js
+++ html/match.js
@@ -1,29 +1,35 @@
 let Match=[
-"Luxemburgo - Portugal", "Belgica - Bielorrusia", "Gibraltar - Paises Bajos",
-" Deportes Tolima - Atlético Nacional "," Jaguares de Córdoba - Junior ",
+"España - Kosovo","Lituania - Italia", "Bosnia - Francia" ,"Inglaterra - Polonia", "Alemania - Macedonia",
+    
+" Banfield - Vélez Sarsfield ",
+ 
+" América de Cali - Millonarios "," Envigado - Patriotas Boyacá "," Rionegro Águilas - Once Caldas ",
 ];
 
 let Est=[
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863166/luxembourg-vs-portugal/ ',
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863167/belgium-vs-belarus/ ',
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863169/gibraltar-vs-netherlands/ ',
-    
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887192/deportes-tolima-vs-atletico-nacional/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887193/jaguares-de-cordoba-vs-junior/ ',
+'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863177/spain-vs-kosovo/',
+'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863178/lithuania-vs-italy/ ',
+'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863180/bosnia-and-herzegovina-vs-france/ ',
+'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863186/england-vs-poland/ ',
+'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3863188/germany-vs-north-macedonia/ ',
+'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3955027/banfield-vs-velez-sarsfield/ ',
+'https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887199/america-de-cali-vs-millonarios/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887194/envigado-vs-patriotas-boyaca/ ','https://pirlotv2.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3887196/rionegro-aguilas-vs-once-caldas/ ',
 ];
 
 let Enl = [
+    "","","","","",
+    "",
     "","","",
-    ""."".
 ];
 
 let Title=[
-"Clasificatorio para la Copa del Mundo","Clasificatorio para la Copa del Mundo","Clasificatorio para la Copa del Mundo",/*"Clasificatorio para la Copa del Mundo","Clasificatorio para la Copa del Mundo",*/
+"Clasificatorio para la Copa del Mundo","Clasificatorio para la Copa del Mundo","Clasificatorio para la Copa del Mundo","Clasificatorio para la Copa del Mundo","Clasificatorio para la Copa del Mundo",
+"Liga Profesional <b><b>(Argentina)</b>",/*"Liga Profesional <b><b>(Argentina)</b>","Liga Profesional <b><b>(Argentina)</b>",*/
 "Liga BetPlay <b>(Colombia)</b>","Liga BetPlay <b>(Colombia)</b>","Liga BetPlay <b>(Colombia)</b>","Liga BetPlay <b>(Colombia)</b>","Liga BetPlay <b>(Colombia)</b>",
 
 "Copa Argentina",/*"Copa Argentina","Copa Argentina",*/
 "FA CUP <b>(Inglaterra)</b>","FA CUP <b>(Inglaterra)</b>",
 "Premier League <b>(Inglaterra)</b>","Premier League <b>(Inglaterra)</b>",
-"Liga Profesional <b><b>(Argentina)</b>","Liga Profesional <b><b>(Argentina)</b>","Liga Profesional <b><b>(Argentina)</b>",
 
 "LaLiga <b><b>(España)</b>","LaLiga <b><b>(España)</b>","LaLiga <b><b>(España)</b>","LaLiga <b><b>(España)</b>","LaLiga <b><b>(España)</b>",
 
