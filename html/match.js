69d00284453872bb471f37bd763925e92fbcdb01
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-04-07 11:59:30 -0300

    Add files via upload

--- html/match.js
+++ html/match.js
@@ -2,6 +2,7 @@ let Match=[
      " Bayern München - PSG "," Porto - Chelsea ",
      " Real Sociedad - Athletic Club ",
      " Internazionale - Sassuolo "," Juventus - Napoli ",
+     " Talleres Córdoba - Vélez Sarsfield "," Atlético Tucumán - River Plate ",
      " Defensa y Justicia - Palmeiras ",
      " Independiente del Valle - Grêmio "," Libertad - Atlético Nacional ",
      " Mineros de Guayana - Aragua "," Nacional Potosí - Guabirá "," Torque - Fénix "," Atlético Palmaflor - Wilstermann "," Sport Huancayo - UTC Cajamarca ",
@@ -13,6 +14,7 @@ let Est=[
     'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3952423/bayern-m-nchen-vs-psg/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3952421/porto-vs-chelsea/ ',
     'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3952437/real-sociedad-vs-athletic-club/ ',
     'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3959574/internazionale-vs-sassuolo/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3787372/juventus-vs-napoli/ ',
+    'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3960543/talleres-cordoba-vs-velez-sarsfield/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3956640/atletico-tucuman-vs-river-plate/ ',
     'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3959115/defensa-y-justicia-vs-palmeiras/ ',
     'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3951610/independiente-del-valle-vs-gremio/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3952445/libertad-vs-atletico-nacional/ ',
     'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917292/mineros-de-guayana-vs-aragua/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917290/nacional-potosi-vs-guabira/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3968463/torque-vs-fenix/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917296/atletico-palmaflor-vs-wilstermann/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917294/sport-huancayo-vs-utc-cajamarca/ ',
@@ -33,6 +35,7 @@ let Title=[
     "Champions League <b>(Cuartos)</b>","Champions League <b>(Cuartos)</b>",
     "LaLiga <b><b>(España)</b>",/*"LaLiga <b><b>(España)</b>","LaLiga <b><b>(España)</b>","LaLiga <b><b>(España)</b>","LaLiga <b><b>(España)</b>",*/
     "Serie A <b><b>(Italia)</b>","Serie A <b><b>(Italia)</b>",/*"Serie A <b><b>(Italia)</b>","Serie A <b><b>(Italia)</b>","Serie A <b><b>(Italia)</b>","Serie A <b><b>(Italia)</b>",*/
+    "Copa Argentina","Copa Argentina",/*"Copa Argentina",*/
     "RECOPA Sudamericana (IDA)",
     "Conmebol Libertadores","Conmebol Libertadores",
     "Conmebol Sudamericana","Conmebol Sudamericana","Conmebol Sudamericana","Conmebol Sudamericana","Conmebol Sudamericana",/*"Conmebol Sudamericana","Conmebol Sudamericana",*/
@@ -43,7 +46,6 @@ let Title=[
     "Copa del Rey <b>(España)</b>",
     "Bundesliga <b><b>(Alemania)</b>","Bundesliga <b><b>(Alemania)</b>",/*"Bundesliga <b><b>(Alemania)</b>","Bundesliga <b><b>(Alemania)</b>","Bundesliga <b><b>(Alemania)</b>",*/
 
-    "Copa Argentina",/*"Copa Argentina","Copa Argentina",*/
     "FA CUP <b>(Inglaterra)</b>","FA CUP <b>(Inglaterra)</b>",
 
     "Clasificatorio para la Copa del Mundo","Clasificatorio para la Copa del Mundo","Clasificatorio para la Copa del Mundo","Clasificatorio para la Copa del Mundo","Clasificatorio para la Copa del Mundo",
