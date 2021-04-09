20c84e299e0a31558729a7cfa5a39ad1b3d40ae5
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-04-08 23:53:08 -0300

    Update html.js

--- html/html.js
+++ html/html.js
@@ -1,4 +1,4 @@
-var HOY = "08/04/2021";let is = [  -1,  -1,  -1, -1,-1,-1];
+var HOY = "09/04/2021";let is = [  -1,  -1,  -1, -1,-1,-1];
 
 document.getElementById("demo2").innerHTML = 
   `<h3 align='center'><u>Calendario ${HOY}</u></h3>` +
