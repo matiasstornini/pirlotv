667437df9abfb176bbc06519ea11a5a94fad2897
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-05-01 17:50:52 -0300

    Update match.js

--- html/match.js
+++ html/match.js
@@ -2,7 +2,7 @@ let Match=[
  " Crystal Palace - Manchester City "," Brighton & Hove Albion - Leeds United "," Chelsea - Fulham ",
   " Eibar - Deportivo Alavés "," Elche - Atlético Madrid "," Huesca - Real Sociedad "," Real Madrid - Osasuna ",
    " Hellas Verona - Spezia "," Crotone - Internazionale "," Milan - Benevento ",
-   " Millonarios - América de Cali "," Deportivo Cali - Deportes Tolima ", 
+   " Millonarios - América de Cali "," Deportivo Cali - Deportes Tolima (Susp.)", 
 ];
 
 let Est=[
@@ -14,8 +14,8 @@ let Est=[
 
 let Enl = [
     "","","",
-    "","","",AEsp[0],
-    "","",AIta[0],
+    "","","","",
+    "","","",
     "","",
     "","","","","","","","","","","","","","","","","","","","",
 ];
