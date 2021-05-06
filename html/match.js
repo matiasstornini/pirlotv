3d6b0a40e3800006bdbbc239981f9c91eb9aaa9a
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-05-06 01:25:35 -0300

    Add files via upload

--- html/match.js
+++ html/match.js
@@ -1,19 +1,19 @@
 let Match=[    
-" Chelsea - Real Madrid ",
- " Independiente del Valle - Universitario "," Racing Club - São Paulo "," Internacional - Olimpia "," Rentistas - Sporting Cristal "," Universidad Católica - Nacional ",
- " Bolívar - Ceará "," Huachipato - Rosario Central "," Torque - Guabirá "," Deportes Tolima - Emelec "," RB Bragantino - Talleres Córdoba ",
+" Arsenal - Villarreal "," Roma - Manchester United ",
+" Always Ready - Deportivo Táchira "," Atlético Nacional - Argentinos Juniors "," Junior - Fluminense "," Santa Fe - River Plate "," Deportivo La Guaira - América de Cali ",
+" Arsenal - Wilstermann "," Grêmio - Aragua "," Libertad - Atlético Goianiense "," Peñarol - River Plate "," San Lorenzo - 12 de Octubre "," Sport Huancayo - Corinthians "," La Equidad - Lanús ",
 ];
 
 let Est=[
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3978416/chelsea-vs-real-madrid/ ',
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3978390/independiente-del-valle-vs-universitario/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3976246/racing-club-vs-sao-paulo/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3976242/internacional-vs-olimpia/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3976245/rentistas-vs-sporting-cristal/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3976247/universidad-catolica-vs-nacional/ ',
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3981092/bolivar-vs-ceara/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3976206/huachipato-vs-rosario-central/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3978992/torque-vs-guabira/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3976213/deportes-tolima-vs-emelec/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3976214/rb-bragantino-vs-talleres-cordoba/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979253/arsenal-vs-villarreal/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979255/roma-vs-manchester-united/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3976241/always-ready-vs-deportivo-tachira/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979269/atletico-nacional-vs-argentinos-juniors/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3980371/junior-vs-fluminense/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3976244/santa-fe-vs-river-plate/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3976250/deportivo-la-guaira-vs-america-de-cali/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3976208/arsenal-vs-wilstermann/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3978994/gremio-vs-aragua/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3978993/libertad-vs-atletico-goianiense/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3978383/penarol-vs-river-plate/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3980367/san-lorenzo-vs-12-de-octubre/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3976211/sport-huancayo-vs-corinthians/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3976215/la-equidad-vs-lanus/ ',
 ];
 
 let Enl = [
     "",
-    "","","","",Extra[0],
-    "","","","",Extra[1],
+    "","","","",
+    "","","","",
 
     "","",
     "","","","","","","","",
@@ -35,8 +35,8 @@ let Title=[
     
     /*"Liga BetPlay <b>(Colombia)</b>","Liga BetPlay <b>(Colombia)</b>",/*"Liga BetPlay <b>(Colombia)</b>","Liga BetPlay <b>(Colombia)</b>","Liga BetPlay <b>(Colombia)</b>","Liga BetPlay <b>(Colombia)</b>","Liga BetPlay <b>(Colombia)</b>","Liga BetPlay <b>(Colombia)</b>",*/
 
-    "Champions League <b>(Semifinal)</b>",/*"Champions League <b>(Cuartos)</b>",*/
-    /*"Europa League <b>(Semifinal)</b>","Europa League <b>(Semifinal)</b>",/*"Europa League <b>(Cuartos)</b>","Europa League <b>(Cuartos)</b>",/*"Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>",*/
+    /*"Champions League <b>(Semifinal)</b>",/*"Champions League <b>(Cuartos)</b>",*/
+    "Europa League <b>(Semifinal)</b>","Europa League <b>(Semifinal)</b>",/*"Europa League <b>(Cuartos)</b>","Europa League <b>(Cuartos)</b>",/*"Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>",*/
     "Conmebol Libertadores","Conmebol Libertadores","Conmebol Libertadores","Conmebol Libertadores","Conmebol Libertadores",/*"Conmebol Libertadores",/*"Conmebol Libertadores",/*"Conmebol Libertadores",*/
     "Conmebol Sudamericana","Conmebol Sudamericana","Conmebol Sudamericana","Conmebol Sudamericana","Conmebol Sudamericana","Conmebol Sudamericana","Conmebol Sudamericana","Conmebol Sudamericana","Conmebol Sudamericana",/*"Conmebol Sudamericana","Conmebol Sudamericana",*/
     
