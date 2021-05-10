9e85703efeffa13bf4ef3ce4fbd06660da1b7373
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-05-10 01:34:14 -0300

    Add files via upload

--- html/match.js
+++ html/match.js
@@ -1,23 +1,13 @@
 let Match=[    
-" Wolverhampton Wanderers - Brighton & Hove Albion "," Aston Villa - Manchester United "," West Ham United - Everton "," Arsenal - West Bromwich Albion ",
-" Getafe - Eibar "," Valencia - Real Valladolid "," Villarreal - Celta de Vigo "," Real Madrid - Sevilla ",
-" Juventus - Milan ",
-" Arsenal - Central Córdoba SdE "," Argentinos Juniors - Estudiantes "," Platense - Rosario Central "," Racing Club - San Lorenzo "," River Plate - Aldosivi "," Colón - Unión Santa Fe "," Huracán - Independiente "," Lanús - Talleres Córdoba "," Newell's Old Boys - Sarmiento ",
-];
+" Real Betis - Granada ",];
 
 let Est=[
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757184/wolverhampton-wanderers-vs-brighton-hove-albion/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757176/aston-villa-vs-manchester-united/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757183/west-ham-united-vs-everton/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757175/arsenal-vs-west-bromwich-albion/ ',
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757538/getafe-vs-eibar/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757542/valencia-vs-real-valladolid/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757543/villarreal-vs-celta-de-vigo/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757540/real-madrid-vs-sevilla/ ',
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777274/juventus-vs-milan/ ',
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917407/arsenal-vs-central-cordoba-sde/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917411/argentinos-juniors-vs-estudiantes/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917412/platense-vs-rosario-central/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917408/racing-club-vs-san-lorenzo/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917410/river-plate-vs-aldosivi/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917419/colon-vs-union-santa-fe/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917414/huracan-vs-independiente/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917415/lanus-vs-talleres-cordoba/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3924464/newells-old-boys-vs-sarmiento/ ',
-
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757537/real-betis-vs-granada/ ',
 ];
 
 let Enl = [
-    "","","","",
-    "","","","",
-    "",
-    "","","","","","","","",AArg[0]
+    "","",
+    
 ];
 
 let Title=[
