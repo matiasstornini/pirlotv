cbf0919a3cd4ea9d1755e7934dcda6ba282cd128
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-05-14 02:23:55 -0300

    Add files via upload

--- html/match.js
+++ html/match.js
@@ -1,22 +1,13 @@
 let Match=[    
-" Manchester United - Liverpool ",
-" Real Valladolid - Villarreal "," Eibar - Real Betis "," Granada - Real Madrid ",
-" Vélez Sarsfield - LDU Quito "," Always Ready - Olimpia "," América de Cali - Atlético Mineiro ",
- " Aragua - La Equidad "," Grêmio - Lanús "," Guabirá - Bahia "," Aucas - Melgar "," Libertad - Newell's Old Boys "," Peñarol - Corinthians "," River Plate - Sport Huancayo ",
+ " Newcastle United - Manchester City ",
 ];
 
 let Est=[
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3993486/manchester-united-vs-liverpool/ ',
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757552/real-valladolid-vs-villarreal/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757546/eibar-vs-real-betis/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757547/granada-vs-real-madrid/ ',
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3977730/velez-sarsfield-vs-ldu-quito/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3978393/always-ready-vs-olimpia/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3978394/america-de-cali-vs-atletico-mineiro/ ',
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3977730/velez-sarsfield-vs-ldu-quito/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3978393/always-ready-vs-olimpia/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3978394/america-de-cali-vs-atletico-mineiro/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757191/newcastle-united-vs-manchester-city/ ',
 ];
 
 let Enl = [
     "",
-    "","","",
-    "",Extra[0],Extra[3],
-    "","","","",Extra[1],Extra[2],""
     
 ];
 
