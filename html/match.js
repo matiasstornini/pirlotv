e0b3995a8acae95658aabb1c8a0ef139478f7809
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-05-19 18:56:03 -0300

    Add files via upload

--- html/match.js
+++ html/match.js
@@ -1,14 +1,14 @@
 let Match=[    
 " Everton - Wolverhampton Wanderers "," Newcastle United - Sheffield United "," Tottenham Hotspur - Aston Villa "," Crystal Palace - Arsenal "," Burnley - Liverpool "," West Bromwich Albion - West Ham United ",
 " Atalanta - Juventus ",
-" Sporting Cristal - Rentistas "," Vélez Sarsfield - Unión La Calera "," Nacional - Universidad Católica "," Cerro Porteño - Atlético Mineiro "," Flamengo - LDU Quito "," River Plate - Santa Fe ",
+" Sporting Cristal - Rentistas "," Vélez Sarsfield - Unión La Calera "," Cerro Porteño - Atlético Mineiro "," Flamengo - LDU Quito "," River Plate - Santa Fe "," América de Cali - Deportivo La Guaira "," Deportivo Táchira - Always Ready ",
 " Atlético Goianiense - Libertad "," Emelec - Deportes Tolima "," Wilstermann - Arsenal "," Athletico-PR - Melgar "," Aucas - Metropolitanos "," Rosario Central - Huachipato ",
 ];
 
 let Est=[
 'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3990347/everton-vs-wolverhampton-wanderers/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3989048/newcastle-united-vs-sheffield-united/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757203/tottenham-hotspur-vs-aston-villa/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757198/crystal-palace-vs-arsenal/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3990344/burnley-vs-liverpool/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757204/west-bromwich-albion-vs-west-ham-united/ ',
 'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3917624/atalanta-vs-juventus/ ',
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979276/sporting-cristal-vs-rentistas/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979277/velez-sarsfield-vs-union-la-calera/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979273/nacional-vs-universidad-catolica/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979280/cerro-porteno-vs-atletico-mineiro/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979279/flamengo-vs-ldu-quito/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979278/river-plate-vs-santa-fe/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979276/sporting-cristal-vs-rentistas/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979277/velez-sarsfield-vs-union-la-calera/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979280/cerro-porteno-vs-atletico-mineiro/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979279/flamengo-vs-ldu-quito/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979278/river-plate-vs-santa-fe/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979282/america-de-cali-vs-deportivo-la-guaira/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979281/deportivo-tachira-vs-always-ready/ ',
 'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979004/atletico-goianiense-vs-libertad/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979002/emelec-vs-deportes-tolima/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979003/wilstermann-vs-arsenal/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979007/athletico-pr-vs-melgar/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979006/aucas-vs-metropolitanos/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979005/rosario-central-vs-huachipato/ ',
 ];
 
@@ -16,7 +16,7 @@ let Enl = [
 
     "","","","","","",
     "",
-    "",Extra[0],"","","","",
+    "",Extra[0],"","","","","",
     "",Extra[1],Extra[2],"","","",
     
 ];
