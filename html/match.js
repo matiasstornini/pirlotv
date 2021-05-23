1838ebbd0866e9f6ad78a757ca7d43deed96bef7
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-05-23 15:26:39 -0300

    Add files via upload

--- html/match.js
+++ html/match.js
@@ -1,20 +1,23 @@
 let Match=[    
  " Arsenal - Brighton & Hove Albion "," Aston Villa - Chelsea "," Fulham - Newcastle United "," Leeds United - West Bromwich Albion "," Leicester City - Tottenham Hotspur "," Liverpool - Crystal Palace "," Manchester City - Everton "," Sheffield United - Burnley "," West Ham United - Southampton ",
  " Granada - Getafe "," Sevilla - Deportivo Alavés ",
+  " Angers SCO - Lille "," Brest - PSG ",
  " Internazionale - Udinese "," Atalanta - Milan "," Bologna - Juventus "," Napoli - Hellas Verona "," Sassuolo - Lazio "," Spezia - Roma ",
 ];
 
 let Est=[
 'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757205/arsenal-vs-brighton-hove-albion/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757206/aston-villa-vs-chelsea/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757207/fulham-vs-newcastle-united/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757208/leeds-united-vs-west-bromwich-albion/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757209/leicester-city-vs-tottenham-hotspur/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757210/liverpool-vs-crystal-palace/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757211/manchester-city-vs-everton/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757212/sheffield-united-vs-burnley/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757213/west-ham-united-vs-southampton/ ',
 'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757566/granada-vs-getafe/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3757571/sevilla-vs-deportivo-alaves/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3715989/angers-sco-vs-lille/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3715990/brest-vs-psg/ ',
 'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777307/internazionale-vs-udinese/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777304/atalanta-vs-milan/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777302/bologna-vs-juventus/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777308/napoli-vs-hellas-verona/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777303/sassuolo-vs-lazio/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3777306/spezia-vs-roma/ ',
 ];
 
 let Enl = [
 
     "","","","","","","","","",
-    AEsp[0],"",
-    "","","","","","",
+    "",AEsp[0],
+    Extra[0],Extra[1]
+    "",AIta[0],AIta[1],Extra[2],"","",
     
 ];
 
@@ -24,6 +27,7 @@ let Title=[
 
     "LaLiga <b>(España)</b>","LaLiga <b>(España)</b>",/*"LaLiga <b>(España)</b>","LaLiga <b>(España)</b>","LaLiga <b>(España)</b>","LaLiga <b>(España)</b>","LaLiga <b>(España)</b>",/*"LaLiga <b>(España)</b>","LaLiga <b>(España)</b>","LaLiga <b>(España)</b>",/*"LaLiga <b>(España)</b>","LaLiga <b>(España)</b>",/*"LaLiga <b>(España)</b>",*/
 
+    "Ligue 1 <b>(Francia)</b>","Ligue 1 <b>(Francia)</b>",
     "Serie A <b>(Italia)</b>","Serie A <b>(Italia)</b>","Serie A <b>(Italia)</b>","Serie A <b>(Italia)</b>","Serie A <b>(Italia)</b>","Serie A <b>(Italia)</b>","Serie A <b>(Italia)</b>","Serie A <b>(Italia)</b>",/*"Serie A <b>(Italia)</b>","Serie A <b>(Italia)</b>",*/
 
     /*"Bundesliga <b>(Alemania)</b>","Bundesliga <b>(Alemania)</b>","Bundesliga <b>(Alemania)</b>","Bundesliga <b>(Alemania)</b>","Bundesliga <b>(Alemania)</b>",/*"Bundesliga <b>(Alemania)</b>",*/
