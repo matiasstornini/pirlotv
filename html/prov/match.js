07e02aa3156ec7d799478de6433db22a87859b13
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-05-26 01:25:30 -0300

    Add files via upload

--- html/prov/match.js
+++ html/prov/match.js
@@ -1,15 +1,19 @@
 let Match=[    
-" River Plate - Fluminense "," Santa Fe - Junior "," Atlético Mineiro - Deportivo La Guaira "," Cerro Porteño - América de Cali "," Racing Club - Rentistas "," São Paulo - Sporting Cristal ",
-" Newell's Old Boys - Atlético Goianiense "," Palestino - Libertad "," Deportes Tolima - RB Bragantino "," Emelec - Talleres Córdoba ",
+" Villarreal - Manchester United ",
+" Internacional - Always Ready "," Olimpia - Deportivo Táchira "," Barcelona - Santos "," Boca Juniors - The Strongest "," Nacional - Argentinos Juniors "," Universidad Católica - Atlético Nacional ",
+ " Bahia - Torque "," Independiente - Guabirá "," Corinthians - River Plate "," Huachipato - San Lorenzo "," Rosario Central - 12 de Octubre "," Sport Huancayo - Peñarol ",
 ];
 
 let Est=[
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979286/river-plate-vs-fluminense/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3986025/santa-fe-vs-junior/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979289/atletico-mineiro-vs-deportivo-la-guaira/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979290/cerro-porteno-vs-america-de-cali/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979287/racing-club-vs-rentistas/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979288/sao-paulo-vs-sporting-cristal/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979013/newells-old-boys-vs-atletico-goianiense/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979014/palestino-vs-libertad/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979016/deportes-tolima-vs-rb-bragantino/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979015/emelec-vs-talleres-cordoba/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3984932/villarreal-vs-manchester-united/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979291/internacional-vs-always-ready/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979292/olimpia-vs-deportivo-tachira/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3998232/barcelona-vs-santos/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979293/boca-juniors-vs-the-strongest/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979295/nacional-vs-argentinos-juniors/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979296/universidad-catolica-vs-atletico-nacional/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3986404/bahia-vs-torque/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979017/independiente-vs-guabira/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979019/corinthians-vs-river-plate/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3987073/huachipato-vs-san-lorenzo/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979018/rosario-central-vs-12-de-octubre/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3979020/sport-huancayo-vs-penarol/ ',
 ];
 
 let Enl = [
 
-    "","","","","","",
+    "",
+    "","","","","",
     "","","","",
     "","","","","","","","","","","",
     
@@ -31,7 +35,7 @@ let Title=[
     /*"Liga BetPlay <b>(Colombia)</b>","Liga BetPlay <b>(Colombia)</b>",/*"Liga BetPlay <b>(Colombia)</b>","Liga BetPlay <b>(Colombia)</b>","Liga BetPlay <b>(Colombia)</b>","Liga BetPlay <b>(Colombia)</b>","Liga BetPlay <b>(Colombia)</b>","Liga BetPlay <b>(Colombia)</b>",*/
 
     /*"Champions League <b>(Semifinal)</b>",/*"Champions League <b>(Cuartos)</b>",*/
-    /*"Europa League <b>(Semifinal)</b>","Europa League <b>(Semifinal)</b>",/*"Europa League <b>(Cuartos)</b>","Europa League <b>(Cuartos)</b>",/*"Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>",*/
+    "Europa League <b>(Final)</b>",/*"Europa League <b>(Semifinal)</b>",/*"Europa League <b>(Cuartos)</b>","Europa League <b>(Cuartos)</b>",/*"Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>","Europa League <b>(Octavos)</b>",*/
     "Conmebol Libertadores","Conmebol Libertadores","Conmebol Libertadores","Conmebol Libertadores","Conmebol Libertadores","Conmebol Libertadores",/*"Conmebol Libertadores",/*"Conmebol Libertadores",*/
     "Conmebol Sudamericana","Conmebol Sudamericana","Conmebol Sudamericana","Conmebol Sudamericana","Conmebol Sudamericana","Conmebol Sudamericana","Conmebol Sudamericana","Conmebol Sudamericana","Conmebol Sudamericana",/*"Conmebol Sudamericana","Conmebol Sudamericana",*/
     
@@ -55,4 +59,3 @@ let Title=[
 
 ];
 //console.log<b>(Est)</b></b>;
- 
