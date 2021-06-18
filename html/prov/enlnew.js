982d410ee4c05dc8d4ffbdb810c9e94eac5768d0
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-06-18 00:48:47 -0300

    Update enlnew.js

--- html/prov/enlnew.js
+++ html/prov/enlnew.js
@@ -1,1 +1,13 @@
 let btn1 = "0";
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+}
+let datef = formatDate;
