79c48b43832c9fc94588b898e3132850a7333925
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-06-18 00:45:00 -0300

    Add files via upload

--- html/match.js
+++ html/match.js
@@ -1,25 +1,22 @@
 let Match=[    
-" Ucrania - North Macedonia "," Dinamarca - Bélgica "," Países Bajos - Austria ",
-" Colombia - Venezuela "," Brasil - Perú ",
- " Deportes Tolima - Millonarios ",
+ " Suecia - Eslovaquia "," Croacia - República Checa "," Inglaterra - Escocia ",
+ " Chile - Bolivia "," Argentina - Uruguay ",
 ];
 
 let Est=[
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3517158/ukraine-vs-north-macedonia/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3517159/denmark-vs-belgium/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3517160/netherlands-vs-austria/ ',
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3794797/colombia-vs-venezuela/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3794799/brazil-vs-peru/ ',
-'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/4023080/deportes-tolima-vs-millonarios/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3517161/sweden-vs-slovakia/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3517162/croatia-vs-czech-republic/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3517163/england-vs-scotland/ ',
+'https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3794794/chile-vs-bolivia/ ','https://pirlotv4.herokuapp.com/full/app/test/statistics?url=https://m.livesoccertv.com/es/match/3794795/argentina-vs-uruguay/ ',
 ];
 
 let Enl = [
 
     "","","",
     "","",
-    "",
 
 
     ""];
 
-let HOY = "17/06/2021";
+let HOY = "18/06/2021";
 let Abtns = [
 1,0,0
 ];
