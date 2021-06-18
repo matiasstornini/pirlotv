a1f7718a66bce344a2eb2c5b2717ea948ee8769d
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-06-18 00:54:56 -0300

    Update enlnew.js

--- html/prov/enlnew.js
+++ html/prov/enlnew.js
@@ -3,9 +3,9 @@ function formatDate(date) {
     var year = date.getFullYear().toString();
     var month = (date.getMonth() + 101).toString().substring(1);
     var day = (date.getDate() + 100).toString().substring(1);
-    return year + "-" + month + "-" + day;
+    return day + "-" + month + "-" + year;
 }
 
 //Usage example:
 //alert(formatDate(new Date()));
-let datef = ormatDate(new Date());
+let datef = formatDate(new Date());
