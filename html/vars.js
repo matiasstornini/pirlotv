ce85b08f221d53f7853e6cd9cbba757baf3a2df6
adiccfut <37271131+adiccfut@users.noreply.github.com>
2022-03-09 16:37:37 -0300

    Update vars.js

--- html/vars.js
+++ html/vars.js
@@ -3,6 +3,7 @@ console.log("hola");
 
 
 var blog = "https://pirlotv-redirecciones.blogspot.com/2020/09/";
+var blog2 = "https://apps-innova-redirects.blogspot.com/2022/03/";
 
 let esp = [
 ""+blog+"espa-0-m3u8.html",""+blog+"esp-0b-m3u8.html",""+blog+"esp-0c-m3u8.html",
@@ -110,5 +111,15 @@ let ext = [
 ""+blog+"extra-0f-m3u8.html",
 ""+blog+"extra-0g-m3u8.html",
 ""+blog+"extra-0h-m3u8.html",
+
+];
+
+let chs = [
+""+blog2+"href-ch-1-m3u8.html",
+""+blog2+"href-ch-2-m3u8.html",
+""+blog2+"href-ch-3-m3u8.html",
+""+blog2+"href-ch-4-m3u8.html",
+""+blog2+"href-ch-5-m3u8.html",
+""+blog2+"href-ch-6-m3u8.html",
 
 ];
