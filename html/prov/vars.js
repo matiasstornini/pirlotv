689fa184091a74ec9e23b5490275f2953782844d
adiccfut <37271131+adiccfut@users.noreply.github.com>
2022-04-26 13:37:58 -0300

    Update vars.js

--- html/prov/vars.js
+++ html/prov/vars.js
@@ -1,125 +1,88 @@
-console.log("hola");
-// var d = new Date();document.getElementById("d").innerHTML = d.getHours()+":"+(d.getMinutes()<10?'0':'') + d.getMinutes();
 
-
-var blog = "https://pirlotv-redirecciones.blogspot.com/2020/09/";
+var blog = "https://apps-innova-redirects.blogspot.com/2020/04/";
+//var blog = "https://pirlotv-redirecciones.blogspot.com/2020/09/";
 var blog2 = "https://apps-innova-redirects.blogspot.com/2022/03/";
 
 let esp = [
-""+blog+"espa-0-m3u8.html",""+blog+"esp-0b-m3u8.html",""+blog+"esp-0c-m3u8.html",
-""+blog+"esp-1-m3u8.html",""+blog+"esp-1b-m3u8.html",""+blog+"esp-1c-m3u8.html",
-""+blog+"esp-2-m3u8.html",""+blog+"esp-2b-m3u8.html",""+blog+"esp-2c-m3u8.html",
-""+blog+"esp-3-m3u8.html",""+blog+"esp-3b-m3u8.html",""+blog+"esp-3c-m3u8.html"
+""+blog+"esp-0.html",""+blog+"esp-0b.html",""+blog+"esp-0c.html",
+""+blog+"esp-1.html",""+blog+"esp-1b.html",""+blog+"esp-1c.html",
+""+blog+"esp-2.html",""+blog+"esp-2b.html",""+blog+"esp-2c.html",
+""+blog+"esp-3.html",""+blog+"esp-3b.html",""+blog+"esp-3c.html"
 
 ];
 
 let ing = [
-""+blog+"premier-0-m3u8.html",""+blog+"premier-0b-m3u8.html",
-""+blog+"premier-1-m3u8.html",""+blog+"premier-1b-m3u8.html",
-""+blog+"premier-2-m3u8.html",""+blog+"premier-2b-m3u8.html"
+""+blog+"ing-0.html",""+blog+"ing-0b.html",
+""+blog+"ing-1.html",""+blog+"ing-1b.html",
+""+blog+"ing-2.html",""+blog+"ing-2b.html"
 ];
 
 let ita = [
-""+blog+"seriea-0-m3u8.html",""+blog+"seriea-0b-m3u8.html",
-""+blog+"seriea-1-m3u8.html",""+blog+"seriea-1b-m3u8.html",
-""+blog+"seriea-1-m3u8.html",""+blog+"seriea-1b-m3u8.html"
+""+blog+"ita-0.html",""+blog+"ita-0b.html",
+""+blog+"ita-1.html",""+blog+"ita-1b.html",
+""+blog+"ita-1.html",""+blog+"ita-1b.html"
 
 ];
 
 let ale = [
-""+blog+"bundes-0-m3u8.html",""+blog+"bundes-0b-m3u8.html",
-""+blog+"bundes-1-m3u8.html",""+blog+"bundes-1b-m3u8.html",
-""+blog+"bundes-2-m3u8.html",""+blog+"bundes-2b-m3u8.html"
+""+blog+"ale-0.html",""+blog+"ale-0b.html",
+""+blog+"ale-1.html",""+blog+"ale-1b.html",
+""+blog+"ale-2.html",""+blog+"ale-2b.html"
 
 ];
 
 let fra = [
-""+blog+"francia-0-m3u8.html",""+blog+"francia-0b-m3u8.html",
-""+blog+"bundes-1-m3u8.html",""+blog+"bundes-1b-m3u8.html",
-""+blog+"bundes-2-m3u8.html",""+blog+"bundes-2b-m3u8.html"
+""+blog+"fra-0.html",""+blog+"fra-0b.html"
 
 ];
 
 let euro = [
-""+blog+"compeuro-0a-m3u8.html",""+blog+"compeuro-0b-m3u8.html",
-""+blog+"conmebol-1-m3u8.html",""+blog+"conmebol-1b-m3u8.html",
-""+blog+"bundes-2-m3u8.html",""+blog+"bundes-2b-m3u8.html"
+""+blog+"euro-0.html",""+blog+"euro-0b.html",
+""+blog+"euro-1.html",""+blog+"euro-1b.html"
 
 ];
 let champions = [
-""+blog+"champions-0-m3u8.html",""+blog+"champions-0b-m3u8.html",""+blog+"champions-0c-m3u8.html",
-""+blog+"champions-1-m3u8.html",""+blog+"champions-1b-m3u8.html",""+blog+"champions-1c-m3u8.html",
-""+blog+"champions-2-m3u8.html",""+blog+"champions-2b-m3u8.html",""+blog+"champions-2c-m3u8.html",
-""+blog+"champions-3-m3u8.html",""+blog+"champions-3b-m3u8.html",""+blog+"champions-3c-m3u8.html"
+""+blog+"champions-0.html",""+blog+"champions-0b.html",""+blog+"champions-0c.html",
+""+blog+"champions-1.html",""+blog+"champions-1b.html",""+blog+"champions-1c.html",
+""+blog+"champions-2.html",""+blog+"champions-2b.html",""+blog+"champions-2c.html",
+""+blog+"champions-3.html",""+blog+"champions-3b.html",""+blog+"champions-3c.html"
 
 ];
 
 let conmebol = [
-""+blog+"conmebol-0-m3u8.html",""+blog+"conmebol-0b-m3u8.html",
-""+blog+"conmebol-1-m3u8.html",""+blog+"conmebol-1b-m3u8.html",""+blog+"conmebol-1c-m3u8.html",
-""+blog+"conmebol-2-m3u8.html",""+blog+"conmebol-2b-m3u8.html"
+""+blog+"conmebol-0.html",""+blog+"conmebol-0b.html",
+""+blog+"conmebol-1.html",""+blog+"conmebol-1b.html",""+blog+"conmebol-1c.html",
+""+blog+"conmebol-2.html",""+blog+"conmebol-2b.html"
 
 ];
 
 let lib = [
-""+blog+"libertadores-href-0.html",""+blog+"libertadores-href-0b.html",""+blog+"libertadores-href-0c.html",
-""+blog+"libertadores-href-1.html",""+blog+"libertadores-href-1b.html",""+blog+"libertadores-href-1c.html",
-""+blog+"libertadores-href-2.html",""+blog+"libertadores-href-2b.html",""+blog+"libertadores-href-2c.html",
-""+blog+"libertadores-href-3.html",""+blog+"libertadores-href-3b.html",""+blog+"libertadores-href-3c.html"
+""+blog+"libertadores-0.html",""+blog+"libertadores-0b.html",""+blog+"libertadores-0c.html",
+""+blog+"libertadores-1.html",""+blog+"libertadores-1b.html",""+blog+"libertadores-1c.html",
+""+blog+"libertadores-2.html",""+blog+"libertadores-2b.html",""+blog+"libertadores-2c.html",
+""+blog+"libertadores-3.html",""+blog+"libertadores-3b.html",""+blog+"libertadores-3c.html"
 ];
 let arg = [
-""+blog+"arg-0-m3u8.html",""+blog+"arg-0b-m3u8.html",""+blog+"arg-0c-m3u8.html",
-""+blog+"arg-1-m3u8.html",""+blog+"arg-1b-m3u8.html",""+blog+"arg-1c-m3u8.html",
-""+blog+"arg-2a.html",""+blog+"arg-2b.html",""+blog+"arg-2c-m3u8.html",
-""+blog+"arg-3-m3u8.html"
+""+blog+"arg-0.html",""+blog+"arg-0b.html",""+blog+"arg-0c.html",
+""+blog+"arg-1.html",""+blog+"arg-1b.html",""+blog+"arg-1c.html",
+""+blog+"arg-2a.html",""+blog+"arg-2b.html",""+blog+"arg-2c.html",
+""+blog+"arg-3.html"
 
 ];
 let col = [
-""+blog+"col-0-m3u8.html",""+blog+"col-0b-m3u8.html",
-""+blog+"col-0c-m3u8.html",
-""+blog+"bundes-2-m3u8.html",""+blog+"bundes-2b-m3u8.html"
+""+blog+"col-0.html",""+blog+"col-0b.html",
+""+blog+"col-0c.html",
+""+blog+"bundes-2.html",""+blog+"bundes-2b.html"
 
 ];
 
-let mex = [
-""+blog+"mexico-0-m3u8.html",""+blog+"mexico-0b-m3u8.html",
-""+blog+"mexico-1-m3u8.html",""+blog+"bundes-1b-m3u8.html",
-""+blog+"bundes-2-m3u8.html",""+blog+"bundes-2b-m3u8.html"
-
-];
-
-let go = [
-""+blog+"go-href-0.html",
-""+blog+"go-href-1.html",
-""+blog+"go-href-2.html",
-""+blog+"go-href-3.html",
-""+blog+"go-href-4.html",
-];
-
-let cpa = [
-"https://bit.ly/3pLvAkY",
-
-""+blog+"go-href-1.html",
-];
-
-let ext = [
-""+blog+"extra-0a-m3u8.html",
-""+blog+"extra-0b-m3u8.html",
-""+blog+"extra-0c-m3u8.html",
-""+blog+"extra-0d-m3u8.html",
-""+blog+"extra-0e-m3u8.html",
-""+blog+"extra-0f-m3u8.html",
-""+blog+"extra-0g-m3u8.html",
-""+blog+"extra-0h-m3u8.html",
-
-];
 
 let chs = [
-""+blog2+"href-ch-1-m3u8.html",
-""+blog2+"href-ch-2-m3u8.html",
-""+blog2+"href-ch-3-m3u8.html",
-""+blog2+"href-ch-4-m3u8.html",
-""+blog2+"href-ch-5-m3u8.html",
-""+blog2+"href-ch-6-m3u8.html",
+""+blog2+"href-ch-1.html",
+""+blog2+"href-ch-2.html",
+""+blog2+"href-ch-3.html",
+""+blog2+"href-ch-4.html",
+""+blog2+"href-ch-5.html",
+""+blog2+"href-ch-6.html",
 
 ];
