e683963fe35f1aeada2f82f7ba1a01861e2776b0
adiccfut <37271131+adiccfut@users.noreply.github.com>
2022-05-12 00:57:38 -0300

    Add files via upload

---
+++ html/prov/App.js
@@ -0,0 +1,45 @@
+const apiurl = 'https://redcardfut2.netlify.app/Datos.json'
+
+fetch(apiurl)
+    .then(response => response.json())
+    .then(function(data) {
+        let html = '';
+        data.forEach(function(data) {
+            var dta = [];
+            var date = new Date(data.dt * 1000);
+            var hours = date.getHours();
+            var minutes = "0" + date.getMinutes();
+            var formattedTime =
+                hours + ":" + minutes.substr(-2); /*+ ':' + seconds.substr(-2)*/
+            dta.push(formattedTime);
+            html += `
+             <h4>${dta} ${data.Partido} (${data.Competicion})</h4>
+                ${data.Switch !== 0 ? (
+                  `
+                  ${data.EnlaceMatchs.valor1 !== "" ? (
+                  	`<a class="waves-effect outline ahref waves-dark btnoutlinedark" href="${data.EnlaceMatchs.valor1}" style="background-color: #ffffff00;"><b>ABRIR 1</b></a>  `
+                    ) : (
+                      ""
+                    )}
+                    ${data.EnlaceMatchs.valor2 !== "" ? (
+                      `<a class="waves-effect outline ahref waves-dark btnoutlinedark" href="${data.EnlaceMatchs.valor2}" style="background-color: #ffffff00;"><b>ABRIR 2</b></a>  `
+                      ) : (
+                        ""
+                      )}
+                      ${data.EnlaceMatchs.valor3 !== "" ? (
+                        `<a class="waves-effect outline ahref waves-dark btnoutlinedark" href="${data.EnlaceMatchs.valor3}" style="background-color: #ffffff00;"><b>ABRIR 3</b></a>  `
+                        ) : (
+                          ""
+                        )}
+                  `
+                ) : (
+                  ""
+                )}
+             <a class="waves-effect outline ahref waves-dark btnoutlinedark" href="${data.Estadisticas}" style="background-color: #ffffff00;">Estadisticas</a>
+             `;
+        })
+        document.getElementById('Matchs').innerHTML = html;
+
+    })
+
+    .catch(err => console.log(err))
--- html/prov/dark-mode.js
+++ html/prov/dark-mode.js
@@ -1,22 +1,25 @@
-function fuctiondarkmode() {
-   var element = document.body;
-   element.classList.toggle("dark-mode");
 
-   if (document.body.classList.contains('dark-mode')) {
-    localStorage.setItem('dark-mode','true');
-   }else{
-    localStorage.setItem('dark-mode','false');
-   }
-}
-if (localStorage.getItem('dark-mode') === 'true') {
-  document.body.classList.add('dark-mode');
-  document.getElementById("buton").innerHTML = "<span onclick='fuctiondarkmode(); location.reload();' class='waves-effect waves-light btnoutline' style='background-color: #ffffff00;''><b>Modo oscuro: ON</b></span>  ";
-}else{
-  document.body.classList.remove('dark-mode');
-  document.getElementById("buton").innerHTML = "<span onclick='fuctiondarkmode(); location.reload();' class='waves-effect waves-light btnoutline' style='background-color: #ffffff00;''><b>Modo claro: ON</b> ";
-}
+  function fuctiondarkmode() {
+     var element = document.body;
+     element.classList.toggle("dark-mode");
 
-document.getElementById("ad-Aec").innerHTML = "<div class='card-panel teal lighten-2'>Serie A de ITALIA por ahora sin transmisiones, estamos trabajando en conseguir las opciones, disculpen las molestia.</div>";
+     if (document.body.classList.contains('dark-mode')) {
+      localStorage.setItem('dark-mode','true');
+     }else{
+      localStorage.setItem('dark-mode','false');
+     }
+  }
+
+  if (localStorage.getItem('dark-mode') === 'true') {
+    document.body.classList.add('dark-mode');
+    document.getElementById("buton").innerHTML = "<span onclick='fuctiondarkmode(); location.reload();' class='waves-effect waves-light btnoutline' style='background-color: #ffffff00;''><b>Modo oscuro: ON</b></span>  ";
+  }else{
+    document.body.classList.remove('dark-mode');
+    document.getElementById("buton").innerHTML = "<span onclick='fuctiondarkmode(); location.reload();' class='waves-effect waves-light btnoutline' style='background-color: #ffffff00;''><b>Modo claro: ON</b> ";
+  }
+
+  let is_dark=document.querySelector("body").classList.contains("dark-mode");let elements=document.getElementsByClassName("waves-effect");for(let i=0;i<elements.length;i++){if(!is_dark){elements[i].classList.remove("waves-light");elements[i].classList.remove("btnoutlinelight");elements[i].classList.add("waves-dark");elements[i].classList.add("btnoutlinedark")}else{elements[i].classList.remove("waves-dark");elements[i].classList.add("waves-light");elements[i].classList.remove("btnoutlinedark");elements[i].classList.add("btnoutlinelight")}}
+
 // 
 let openScreen = (screenName) => {
   // any other stuff you may need to do before opening the screen.
